94dc4bd5397c19514d200aacb4c18899a4242cad
jk-nishad-72 <jkn18869@gmail.com>
2025-09-12 09:09:16 +0530

    D1 basics

---
+++ Daywise_solutions/Day_01_Basics_Of_Js/app.js
@@ -0,0 +1,15 @@
+
+
+let a = 10 ;
+let b = 20 ;
+
+console.log(a+b);
+
+// * guess the output 
+console.log(a+"addition "+ a + b)
+
+
+let Name = prompt('enter your name');
+
+console.log('namaste'+Name)
+
