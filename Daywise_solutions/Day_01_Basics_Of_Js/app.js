5243ff787d67945b93a126f610f7858d8f8609dc
jk-nishad-72 <jkn18869@gmail.com>
2025-09-12 22:24:51 +0530

    D1 completed

--- Daywise_solutions/Day_01_Basics_Of_Js/app.js
+++ Daywise_solutions/Day_01_Basics_Of_Js/app.js
@@ -1,5 +1,9 @@
 
 
+
+
+// * Q1 sum of two integer 
+
 let a = 10 ;
 let b = 20 ;
 
@@ -9,7 +13,62 @@ console.log(a+b);
 console.log(a+"addition "+ a + b)
 
 
-let Name = prompt('enter your name');
+// * Q2 take input from user and greet them with their name
+let Name = prompt('Enter your Name :- ');
 
-console.log('namaste'+Name)
+console.log('namaste 🙏 '+Name)
+
+ const container = document.querySelector('.container');
+    container.innerHTML = `<h1> Namaste 🙏 ${Name} </h1>` 
+
+
+
+ /*
+  * Q3 swapping of two numbers 
+  *  It can be solved in 3 ways
+  *  1. using third variable 
+  *  2. without using third variable (arithmetic operation)
+  *  3. using destructuring 
+
+ */
+
+// 1. using third variable
+
+let x  = 5;
+let y = 10 ;
+
+let temp = x ;
+x = y ;
+y = temp;
+
+console.log('the values of x and y after swapping is '+x +'and'+ y);
+
+// 2. without using third variable (arithmetic operation) {now the x = 10 and y = 5}
+
+  x = x + y;
+  y = x -y;
+  x = x-y;
+console.log('the values of x and y after swapping is '+x +'and'+ y);
+
+// 3. using destructuring { now the x = 5 and y = 10}
+    [x,y]=   [y ,x]
+
+console.log('the values of x and y after swapping is '+x +'and'+ y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
 
