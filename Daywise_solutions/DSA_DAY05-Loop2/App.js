8bf3e73b9906804c5995e167098ffe68b99c8168
jk-nishad-72 <jkn18869@gmail.com>
2025-09-16 23:47:57 +0530

    DSA D5 WHILE LOOP DONE

--- Daywise_solutions/DSA_DAY05-Loop2/App.js
+++ Daywise_solutions/DSA_DAY05-Loop2/App.js
@@ -1,1 +1,83 @@
+let n  = Number(prompt("Enter a number"))
 
+
+
+
+
+
+
+//*✅ while loop: Print numbers from 1 to 5
+
+let i = 0;
+while(i<=5){
+    console.log(i)
+    i++
+}
+
+
+//*✅ Q. Sum of Digits
+
+function sumOfDigits(n) {
+    // Write your logic here
+    let sum =0;
+    while(n>0){
+        
+         sum += Math.floor(n%10)
+         n = Math.floor(n/10)
+    }
+    
+    return sum 
+    
+}
+
+console.log(sumOfDigits(n))
+
+
+
+//*✅ Q. Reverse of a Number
+
+
+function ReverseOfDigits(n) {
+    
+     let revers = 0;
+     let rem =0;
+     while(n>0){
+        
+        rem = n%10
+        revers = (revers*10 )+rem
+       
+        n = Math.floor(n/10)
+    
+     }
+
+     return revers
+      
+}
+console.log(ReverseOfDigits(n))
+
+
+ 
+
+
+function isAutomorphic(n) {
+    // Write your logic here
+    
+let copy =n;
+let squar = n*n;
+
+  let count = 0;
+  
+  while(n>0){
+       count++
+       console.log(count)
+       n = Math.floor(n/10)
+  }
+  
+console.log(copy%(Math.pow(10,count)))
+   
+  if(copy == squar%(Math.pow(10,count))) return "Yes"
+  
+  else return "No"
+}
+
+console.log(isAutomorphic(n))
