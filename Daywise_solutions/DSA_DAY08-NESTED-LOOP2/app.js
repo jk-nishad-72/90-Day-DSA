671c1ef3405342804582d23a00101bc95b3da812
jk-nishad-72 <jkn18869@gmail.com>
2025-09-19 23:56:00 +0530

    DSA D8 NESTED LOOP 2

---
+++ Daywise_solutions/DSA_DAY08-NESTED-LOOP2/app.js
@@ -0,0 +1,99 @@
+/**
+ * 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ */
+
+let prompt = require('prompt-sync')()
+
+let n = Number(prompt("Enter a Number : "))
+
+
+for(let i =1;i<=n;i++){
+
+for(let j = 1;j<=n-i;j++)
+{
+     process.stdout.write(" " ) 
+}
+for(let k =1;k<=i;k++){
+      process.stdout.write("*") 
+}
+
+console.log();
+
+}
+
+
+
+function printMirroredRightTriangle(n) {
+   
+    
+    for(let i =1;i<=n;i++){
+        
+        for(let j =1; j<=n-i;j++){
+            
+             process.stdout.write('  ' )
+        }
+        for(let k =1;k<=i;k++){
+            process.stdout.write('* ')
+        }
+        
+        console.log()
+    }
+    
+}
+
+console.log(printMirroredRightTriangle(n));
+
+
+
+
+
+/**
+ *  Q21
+ *
+ */
+
+
+function printVShapePattern(n) {
+   
+    
+for(let i =1;i<=n;i++){
+    
+    for(let j = 1;j<= n*2-1;j++){
+        if(j==i || i+j == n*2) process.stdout.write('* ')
+        
+        else process.stdout.write('  ')
+    }
+    console.log()
+}    
+    
+    
+}
+
+console.log((printVShapePattern(n)));
+
+
+
+
+// * 22
+
+function printXShapePattern(n) {
+    // Write your logic here
+    for(let i = 1;i<=n;i++){
+        
+       for(let j = 1;j<=n;j++){
+            if(j==i || j+i == n+1) process.stdout.write("*")
+        else process.stdout.write(" ")
+       }
+        
+        console.log()
+    }
+}
+
+console.log(printXShapePattern(n));
+
+
