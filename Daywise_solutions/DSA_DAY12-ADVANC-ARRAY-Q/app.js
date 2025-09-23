a0990ec959b6ade2f36657ede70f5600b3ef32b9
jk-nishad-72 <jkn18869@gmail.com>
2025-09-23 22:45:48 +0530

    linear search done

--- Daywise_solutions/DSA_DAY12-ADVANC-ARRAY-Q/app.js
+++ Daywise_solutions/DSA_DAY12-ADVANC-ARRAY-Q/app.js
@@ -1,1 +1,23 @@
 
+
+
+
+// * — Q 30. Linear Search an array - If element found print the index else -1
+
+
+function linearSearch(arr, target) {
+   
+   let key = -1
+    for(let i =  0;i<arr.length;i++){
+        
+        if(arr[i] === target) {
+            
+             key = i
+             break
+        }
+    }
+    return key
+}
+
+
+console.log(linearSearch([1,2,3,4,5],6))
