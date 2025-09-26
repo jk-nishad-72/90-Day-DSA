a8bbff1c7755b55947896fc4e14563eec9b571f4
jk-nishad-72 <jkn18869@gmail.com>
2025-09-26 23:57:40 +0530

    leedcode que done

--- Daywise_solutions/DSA_DAY15-2DARRAY-AND-LEEDCODE/app.js
+++ Daywise_solutions/DSA_DAY15-2DARRAY-AND-LEEDCODE/app.js
@@ -1,1 +1,34 @@
 
+
+//* —Q 31. 1572. Matrix Diagonal Sum
+
+function diagonalSum(mat) {
+    // Write your logic here
+    
+    
+    let lerfsum =0;let rightsum=0;
+    for(let i = 0;i<mat.length;i++){
+        
+        for(let j = 0;j<mat[i].length;j++){
+            
+            
+            if(i==j){
+                
+                lerfsum += mat[i][j];
+            }
+            
+            if(i==j&&i+j==mat.length-1){
+                continue;
+            }
+            
+            if(i+j==mat.length-1){
+                
+                rightsum += mat[i][j];
+            }
+        }
+        
+    }
+    
+    return rightsum+lerfsum
+    
+}
