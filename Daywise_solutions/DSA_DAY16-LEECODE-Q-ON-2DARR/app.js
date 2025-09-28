60fe3ad4286617fd75abde18692a66533131cd5a
jk-nishad-72 <jkn18869@gmail.com>
2025-09-28 23:52:58 +0530

    DSA 17 SPIRAL MATRIX

--- Daywise_solutions/DSA_DAY16-LEECODE-Q-ON-2DARR/app.js
+++ Daywise_solutions/DSA_DAY16-LEECODE-Q-ON-2DARR/app.js
@@ -66,3 +66,33 @@ console.log(rotate(arr));
 
 
 
+
+var spiralOrder = function(matrix) {
+ 
+    let minc = 0; let maxc = matrix[0].length-1;
+    let minr =0;let maxr = matrix.length-1;
+    let ans = []
+    let m = matrix.length;let n = matrix[0].length
+
+    while(ans.length<m*n){
+        for(let i = minc;i<=maxc;i++){
+            ans.push(matrix[minr][i])
+        }
+        minr++
+        for(let i = minr;i<=maxr;i++){
+            ans.push(matrix[i][maxc])
+        }
+        maxc--
+        for(let i = maxc;i>=minc;i--){
+            ans.push(matrix[maxr][i])
+        }
+        maxr--
+        for(let i = maxr;i>=minr;i--){
+            ans.push(matrix[i][minc])
+        }
+        minc++
+    }
+
+ return ans 
+    
+};
