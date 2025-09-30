ade955f1c1b5c025beec86f2fb69f86ea28858c7
jk-nishad-72 <jkn18869@gmail.com>
2025-09-30 22:44:30 +0530

    Q 1 DONE

--- Daywise_solutions/DSA_DAY19-MORE-ON-STRING/app.js
+++ Daywise_solutions/DSA_DAY19-MORE-ON-STRING/app.js
@@ -1,9 +1,47 @@
 
 let prompt = require('prompt-sync')()
 
+
+/**
+ * 
+  **— Q 58. Toggle each alphabet of String
+  **In - AcgDfD Output - aCGdFd
+
+ */
+
+function ToggleEachChar(){
+    
 let s = prompt('Enter String : ')
 
-for(let i = 0;i<s.length;i++){
+ let output = ''
 
-     console.log(s.charCodeAt(i));
+for(let i = 0;i<s.length;i++){
+ 
+     let asci = s.charCodeAt(i)
+
+     if(asci>=65&&asci<=90){
+         
+        output += String.fromCharCode(asci+32)
+
+     }else if(asci>=97&&asci<=122){
+
+        output += String.fromCharCode(asci-32)
+
+     }
 }
+
+return output
+
+}
+
+console.log(ToggleEachChar());
+
+
+/**
+ * — Q 59. Take an array of strings words and a String Prefix. Print the number of strings
+in words that contain pref as a prefix.
+Example - Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+ */
+
+
