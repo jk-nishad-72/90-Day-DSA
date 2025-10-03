4245380a30ceed04e03151ddd096b1e61bac243e
jk-nishad-72 <jkn18869@gmail.com>
2025-10-04 00:07:40 +0530

    leetcode q solve

--- Daywise_solutions/DSA_DAY22-QUE-ON-MAP-A-BITS/app.js
+++ Daywise_solutions/DSA_DAY22-QUE-ON-MAP-A-BITS/app.js
@@ -1,7 +1,43 @@
 
+// *—Q 67.Two Sum ✔️
+var twoSum = function(nums, target){    
+    let map = new Map();
+      let ans = [-1,-1]
+    for(let i =0;i<nums.length;i++){
+        if(map.has(target-nums[i])){
+            ans[0]=map.get(target-nums[i])
+            ans[1] =i
+            return ans 
+        }else{
+            map.set(nums[i],i)
+        }
+    }
+};
+
+
+
+
+//*—Q 68.First letter appears twice ✔️
+
+var repeatedCharacter = function(s) {
+    let map = new Map();
+    for(let i = 0;s.length;i++){
+     let ch = s.charAt(i);
+    if(map.has(ch)){
+        map.set(ch,map.get(ch)+1)
+        if(map.get(ch)==2) return ch;
+
+       }else{
+        map.set(ch,1)
+         }
+    }
+    
+};
 
 
 //*—Q 69.Sort the people
+
+
 
 var sortPeople = function(names, heights) {
     let map = new Map()
