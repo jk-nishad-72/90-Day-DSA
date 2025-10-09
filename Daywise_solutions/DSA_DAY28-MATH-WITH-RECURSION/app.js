af2aa14b337c3b8aa76682fe5176001a49bdd66d
jk-nishad-72 <jkn18869@gmail.com>
2025-10-09 23:13:01 +0530

    third method done

--- Daywise_solutions/DSA_DAY28-MATH-WITH-RECURSION/app.js
+++ Daywise_solutions/DSA_DAY28-MATH-WITH-RECURSION/app.js
@@ -59,7 +59,30 @@ function gcd2a() {
     console.log(`GCD of given two numbers is ${gcd2b(a,b)}`);
 
   }
-gcdMethod2();
+// gcdMethod2();
+
+
+
+
+//*3.) Using Euclidean Algorithm TC (O(logn)) ➡️
+
+function gcd3() {
+    let a = Number(prompt('Enter first number: '));
+    let b = Number(prompt('Enter second number: '));
+
+    function gcdAlgo(a,b) {
+        
+        return b==0 ? a : gcdAlgo(b, a%b);
+        
+    }
+
+    console.log(`GCD of given two numbers is ${gcdAlgo(a,b)}`);
+
+}
+
+gcd3();
+
+
 
 
 
