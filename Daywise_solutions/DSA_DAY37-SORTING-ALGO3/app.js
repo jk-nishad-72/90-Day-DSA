aa156ffc6464d42387faa1d199e4efbf60790a13
jk-nishad-72 <jkn18869@gmail.com>
2025-10-18 20:43:43 +0530

    cyclic 0 to n done

--- Daywise_solutions/DSA_DAY37-SORTING-ALGO3/app.js
+++ Daywise_solutions/DSA_DAY37-SORTING-ALGO3/app.js
@@ -1,3 +1,48 @@
 
 
+//*— Cyclic Sort  ☑️ [0 ot n] 
+
+let prompt  = require('prompt-sync')();
+
+let size  = parseInt(prompt('Enter Array size: '))
+
+let nums = new Array(size)
+
+
+for(let i  = 0;i<size;i++){
+
+     nums[i] = parseInt(prompt(`Enter elements between 0 to n  ${i+1} :  `))
+
+}
+
+console.log(nums);
+
+var swap = function(arr,i,j){
+
+     let temp = arr[i] ;
+     arr[i] = arr[j];
+     arr[j] = temp
+}
+
+
+function CyclicSort(nums){
+
+    let i = 0;
+
+     while(i<nums.length){
+
+         let correctIndex = nums[i] 
+         if(nums[correctIndex]!=nums[i]){
+             swap(nums ,correctIndex,i)
+             
+         }else{
+            i++
+         }
+     }
+    
+    return nums
+}
+
+console.log(CyclicSort(nums));
+
 
