bee372c86578335cc8cb0049068eb16d8f0f3205
jk-nishad-72 <jkn18869@gmail.com>
2025-10-19 00:39:06 +0530

    D37 done

--- Daywise_solutions/DSA_DAY37-SORTING-ALGO3/app.js
+++ Daywise_solutions/DSA_DAY37-SORTING-ALGO3/app.js
@@ -1,69 +1,97 @@
+let prompt = require("prompt-sync")();
 
+let size = parseInt(prompt("Enter Array size: "));
 
-//*— Cyclic Sort  ☑️ [0 ot n] 
+let nums = new Array(size);
 
-let prompt  = require('prompt-sync')();
-
-let size  = parseInt(prompt('Enter Array size: '))
-
-let nums = new Array(size)
-
-
-for(let i  = 0;i<size;i++){
-
-     nums[i] = parseInt(prompt(`Enter elements between 0 to n  ${i+1} :  `))
-
+for (let i = 0; i < size; i++) {
+  nums[i] = parseInt(prompt(`Enter elements between 0 to n  ${i + 1} :  `));
 }
 
 console.log(nums);
 
-var swap = function(arr,i,j){
+//*— Cyclic Sort  ☑️ [0 ot n]
 
-     let temp = arr[i] ;
-     arr[i] = arr[j];
-     arr[j] = temp
+var swap = function (arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+function CyclicSort(nums) {
+  let i = 0;
+
+  while (i < nums.length) {
+    let correctIndex = nums[i];
+    if (nums[correctIndex] != nums[i]) {
+      swap(nums, correctIndex, i);
+    } else {
+      i++;
+    }
+  }
+  return nums;
 }
 
-function CyclicSort(nums){
+console.log(CyclicSort(nums));
 
-    let i = 0;
 
-     while(i<nums.length){
 
-         let correctIndex = nums[i] 
-         if(nums[correctIndex]!=nums[i]){
-             swap(nums ,correctIndex,i)
-             
-         }else{
+//*—Q94. 268. Missing Number Optimise cyclic solution ✅
+
+var missingNumber = function(nums) {
+   let i = 0;
+   while(i<nums.length){
+    let correctIndex = nums[i] 
+          if(nums[i]<nums.length && nums[correctIndex] != nums[i]){
+            swap(nums,correctIndex,i)
+          } else{
             i++
-         }
-     }
-    return nums
-}
+          }
+   }
+   for(let i = 0;i<nums.length;i++){
+      
+       if(nums[i]!=i) return i
+   }
 
-// console.log(CyclicSort(nums));
+   return nums.length
+
+    
+};
 
 
-var firstMissingNUm = (nums)=>{
+//*—Q95. 448. Find all the number disappeared in an array Using cyclic sort ✅
 
+var findDisappearedNumbers = function(nums) {
+ let ans = []
+ let i = 0;
+   while(i<nums.length){
+    let correctIndex = nums[i]-1
+          if(nums[correctIndex] != nums[i]){
+            swap(nums,correctIndex,i)
+          } else i++  
+   }
+   for(let i = 0;i<nums.length;i++){
+       if(nums[i]!=i+1)  ans.push(i+1)
+   }
+   return ans
+};
 
-    let i = 0;
-    while(i<nums.length){
-         
-         let correctIndex = nums[i]-1;
-         if(nums[i]>0 && nums[i]< nums.length && nums[i] != nums[correctIndex]){
-             swap(nums,correctIndex,i)
-         }else i++
-    }
+//*—Q96. 41.  Find first missing Number .✅
 
-    for(let i = 0;i<nums.length;i++){
-        if(nums[i] !=i+1) return i+1
-    }
+var firstMissingNUm = (nums) => {
+  let i = 0;
+  while (i < nums.length) {
+    let correctIndex = nums[i] - 1;
+    if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correctIndex]) {
+      swap(nums, correctIndex, i);
+    } else i++;
+  }
 
-     return nums.length+1
-}
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != i + 1) return i + 1;
+  }
+
+  return nums.length + 1;
+};
 
 console.log(firstMissingNUm(nums));
-
-
-
