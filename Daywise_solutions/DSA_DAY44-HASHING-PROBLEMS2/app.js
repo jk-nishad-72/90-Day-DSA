002b15f6cba407cb60caabb94fc0808d44fa7e3b
jk-nishad-72 <jkn18869@gmail.com>
2025-10-25 09:51:39 +0530

    DSA D44

---
+++ Daywise_solutions/DSA_DAY44-HASHING-PROBLEMS2/app.js
@@ -0,0 +1,22 @@
+
+//* 350. Intersection of Two Arrays II  
+
+var intersect = function(nums1, nums2) {
+    let map = new Map();let ans = [];
+    for(let i =0;i<nums1.length;i++){   
+         if(map.has(nums1[i])){
+            map.set(nums1[i],map.get(nums1[i])+1)
+         }else {
+            map.set(nums1[i],1)
+         }
+    } 
+    for(let i =0;i<nums2.length;i++){
+        if(map.get(nums2[i])>0){
+            ans.push(nums2[i]);
+            map.set(nums2[i],map.get(nums2[i])-1)
+        }
+    }
+   return ans
+};
+
+
