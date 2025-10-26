5c12191f779b3d8fa7c7471ff76c8f84e0fabe30
jk-nishad-72 <jkn18869@gmail.com>
2025-10-26 09:53:37 +0530

    DSA D45 HASHING SLIDING WINDOW

---
+++ Daywise_solutions/DSA_DAY44-HASHING-PROBLEMS2/practice.js
@@ -0,0 +1,34 @@
+
+
+let prompt = require('prompt-sync')();
+
+let n = parseInt(prompt('Enter a Number : '))
+
+
+
+// for(let i = 1;i<=n;i++){
+
+//       let char = 97;
+
+//     for(let j = 1;j<=i;j++){
+//          process.stdout.write(String.fromCharCode(char) + " ")
+//          char++
+// }
+// console.log();
+
+// }
+
+
+// for(let i = 1;i<=n;i++){
+
+//     //  let char = 65;
+//      let char = 97;
+//     for(let j =  1;j<=n-i+1;j++){
+//         process.stdout.write(String.fromCharCode(char)+ " ")
+//         char++
+//     }
+//     console.log();
+    
+// }
+
+
