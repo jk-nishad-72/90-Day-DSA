0ba37e4c66b8153a40061a7ffd4b48ad9d81f2fd
jk-nishad-72 <jkn18869@gmail.com>
2025-10-28 22:29:33 +0530

    insert at first done

--- Daywise_solutions/DSA_DAY47-LINKED-LIST/app.js
+++ Daywise_solutions/DSA_DAY47-LINKED-LIST/app.js
@@ -1,1 +1,90 @@
 
+
+// * class for creation nodes 
+
+class Node{
+     
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+
+}
+
+
+const n1 = new Node(50);
+
+console.log(n1);
+
+//* for perform linked list operation 
+
+
+class linkedList{
+
+    //* for head Pointer 
+    constructor() {
+        this.head  = null;
+        this.size = 0;
+    }
+
+  //* Insert operation ->
+
+  //* 1.a) Insert At first position 
+
+
+  
+  InsertAtFirst(val){
+
+      this.size++
+
+    const newNode = new Node(val);
+
+      if(this.head === null){
+        this.head = newNode;
+        return
+      }
+      newNode.next = this.head;
+      this.head = newNode;
+  }
+
+  //* 2.  Deletion Opertion 
+
+  //* 2
+
+//* Travarsal Operation of linked list 
+
+
+  printLL(){
+     if(this.head===null){
+        console.log('Empty Linked list ');
+         return
+     }
+   let temp = this.head;
+
+   while(temp!=null){
+    process.stdout.write(' '+temp.val + " -->"); 
+    temp = temp.next;
+   }
+  } 
+
+
+
+}
+
+//* initially ll is empty so head has null 
+
+const ll1 = new linkedList();
+
+console.log(ll1);
+
+
+ll1.InsertAtFirst(10)
+ll1.InsertAtFirst(20)
+ll1.InsertAtFirst(30)
+ll1.InsertAtFirst(40)
+
+
+ll1.printLL()
+
+
+
