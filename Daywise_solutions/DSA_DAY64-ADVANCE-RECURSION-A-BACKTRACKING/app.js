5ba280321cabdfdbe4594ae8bd293579c6897c8c
jk-nishad-72 <jkn18869@gmail.com>
2025-11-14 23:50:28 +0530

    DSA D64 ADVANCE RECURSION AND BACKTRACKING

---
+++ Daywise_solutions/DSA_DAY64-ADVANCE-RECURSION-A-BACKTRACKING/app.js
@@ -0,0 +1,24 @@
+
+
+/*
+ * 1823. Find the Winner of the Circular Game
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+
+var solve = function(k,arr,i){
+    if(arr.length === 1 ) return arr[0]
+    i = (i+k) % arr.length;
+    arr.splice(i,1)
+    return solve(k,arr,i)
+}
+var findTheWinner = function(n, k) {
+    let arr = new Array(n);
+    for(let i = 0;i<n;i++){
+          arr[i] = i+1;
+    }
+    k--
+ return solve(k,arr,0)
+    
+};
