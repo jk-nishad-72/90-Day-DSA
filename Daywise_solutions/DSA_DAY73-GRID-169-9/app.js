80f3a45e24d189d26d2f3089e648b5a3ff31782b
jk-nishad-72 <jkn18869@gmail.com>
2025-11-23 22:46:00 +0530

    DSA D73 DONE

--- Daywise_solutions/DSA_DAY73-GRID-169-9/app.js
+++ Daywise_solutions/DSA_DAY73-GRID-169-9/app.js
@@ -3,21 +3,50 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    let copy = x
-    let temp = 0;
-    while(x > 0){    
-         let rem = x % 10 ;
+// var isPalindrome = function(x) {
+//     let copy = x
+//     let temp = 0;
+//     while(x > 0){    
+//          let rem = x % 10 ;
 
-         temp = (temp * 10 )+rem;
+//          temp = (temp * 10 )+rem;
 
-         x = parseInt(x / 10)
-    }
- if(copy === temp) return true
- else return false
+//          x = parseInt(x / 10)
+//     }
+//  if(copy === temp) return true
+//  else return false
+ 
+// };
+
+// console.log(isPalindrome(121));
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+    let clr = s.replace(/[^a-zA-Z0-9]/gi, "").toLowerCase()
+
+     let i = 0; 
+     let j = clr.length-1;
+
+     while(i <= j){
+
+         if(clr.charAt(i) !== clr.charAt(j)){
+                  return false
+         }else{
+             i++
+             j--
+         }
+     }
+     return true
  
 };
 
-console.log(isPalindrome(121));
+console.log(isPalindrome("race a car"));
+
+
+
 
 
