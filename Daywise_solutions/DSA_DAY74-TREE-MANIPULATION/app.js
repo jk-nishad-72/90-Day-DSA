ca8b3f8255c9877594c97c149f4e2a9bbccb9e99
jk-nishad-72 <jkn18869@gmail.com>
2025-11-25 00:16:47 +0530

    dsa d74 done

--- Daywise_solutions/DSA_DAY74-TREE-MANIPULATION/app.js
+++ Daywise_solutions/DSA_DAY74-TREE-MANIPULATION/app.js
@@ -1,1 +1,39 @@
+ let prompt = require('prompt-sync')()
+
+class Node{
+
+     constructor(val) {
+          this.val = val;
+          this.right = this.left = null
+        
+     }
+}
+
+class Tree {
+
+     buildTree(){
+
+         let data  = parseInt(prompt('Enter Value : '))
+
+         if(data == -1 ) return null
+         
+          let root = new Node(data);
+
+          console.log('Enter left data of '+ root.val);
+          
+          root.left = this.buildTree();
+
+          console.log('Enter right data of '+ root.val);
+
+           root.right = this.buildTree();
+           
+           return root;
+     }
+}
+
+
+const T1 = new Tree()
+
+console.log(T1.buildTree());
+
 
