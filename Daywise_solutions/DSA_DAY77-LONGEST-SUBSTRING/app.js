da184fdaf2bb8ab7aa7dff2cf445821cc3e4702c
jk-nishad-72 <jkn18869@gmail.com>
2025-11-27 23:15:14 +0530

    DONE D77

--- Daywise_solutions/DSA_DAY77-LONGEST-SUBSTRING/app.js
+++ Daywise_solutions/DSA_DAY77-LONGEST-SUBSTRING/app.js
@@ -2,7 +2,7 @@
 let prompt = require('prompt-sync')();
 
 
-
+//*977. Squares of a Sorted Array  ☑️
 var sortedSquares = function(nums){
 
     let ans = new Array()
@@ -21,3 +21,26 @@ console.log(sortedSquares( [-4,-1,0,3,10]));
 
 
 
+
+/*
+ *136. Single Number ✅
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+      let map = new Map()
+      for(let i = 0;i<nums.length;i++){
+           if(map.has(nums[i])) {       
+              map.set(nums[i],map.get(nums[i])+1)
+           }else{
+            map.set(nums[i],1)
+           }
+      }
+      for(let i = 0;i<nums.length;i++){
+         
+         if( map.get(nums[i]) == 1) return nums[i]
+      }
+};
+
+
+console.log(singleNumber([4,1,2,1,2]));
