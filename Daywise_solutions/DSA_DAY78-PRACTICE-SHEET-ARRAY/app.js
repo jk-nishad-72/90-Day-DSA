1db71dbedaf6049505df367ec640aa8030daa4ae
jk-nishad-72 <jkn18869@gmail.com>
2025-11-28 22:07:56 +0530

    D78 DONE

--- Daywise_solutions/DSA_DAY78-PRACTICE-SHEET-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY78-PRACTICE-SHEET-ARRAY/app.js
@@ -1,3 +1,22 @@
+/*
+ *1929. Concatenation of Array ✅
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let n = nums.length;
+    let ans = new Array(n*2)
+
+    for(let i = 0;i< n ;i++){
+         ans[i]  = ans[i+n] = nums[i]
+    }
+    return ans
+};
+
+console.log(getConcatenation([1,3,2,1]));
+
+
+
 
 /*
  *1920. Build Array from Permutation ✅
@@ -15,3 +34,5 @@ var buildArray = function(nums) {
 
     return ans
 };
+
+console.log(buildArray([5,0,1,2,3,4]));
