4c8385d9b1bc3c38a30040edca85af861d13f6f7
jk-nishad-72 <jkn18869@gmail.com>
2025-11-28 22:05:21 +0530

    DSA DAY 78 PRACTICE SHEET -ARRAY Q

---
+++ Daywise_solutions/DSA_DAY78-PRACTICE-SHEET-ARRAY/app.js
@@ -0,0 +1,17 @@
+
+/*
+ *1920. Build Array from Permutation ✅
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    
+    let ans = new Array(nums.length)
+
+    for(let i = 0;i<nums.length; i++){
+         
+          ans[i] = nums[nums[i]];
+    }
+
+    return ans
+};
