015d539060b1e69f527bfe3838b843ad8b0d6682
jk-nishad-72 <jkn18869@gmail.com>
2025-11-29 22:44:35 +0530

    112 DONE

--- Daywise_solutions/DSA_DAY79-BINARY-TREE-BFS/app.js
+++ Daywise_solutions/DSA_DAY79-BINARY-TREE-BFS/app.js
@@ -30,4 +30,38 @@ var invertTree = function(root) {
 console.log(invertTree([2,1,3]));
 
 
+/*
+ *112. Path Sum
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+var solve = function(r,t){
+     if(r == null ) return false 
+     if(r.left == null && r.right == null && r.val == t) return true
+
+  let l = solve(r.left , t - r.val)
+
+  let right = solve(r.right, t - r.val)
+
+  return l || right
+
+ }
+var hasPathSum = function(root, targetSum) {
+    
+   return solve(root, targetSum)
+};
+
+console.log(hasPathSum([1,2,3], 5));
+
+
 
