a74dec2b15ccfa1e66d2dd3ba776858a169482a9
jk-nishad-72 <jkn18869@gmail.com>
2025-11-30 16:00:47 +0530

    107 DONE

--- Daywise_solutions/DSA_DAY80-ASSINGMENT-ON-BT/app.js
+++ Daywise_solutions/DSA_DAY80-ASSINGMENT-ON-BT/app.js
@@ -26,3 +26,39 @@ var isSameTree = function(p, q) {
   if(p == null && q == null) return true
   return solve(p,q)
 };
+
+
+
+/**
+ *107. Binary Tree Level Order Traversal II
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/*
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root){
+    let ans = [];
+    if(root == null ) return ans
+    let q = [];
+    q.push(root)
+    while(q.length != 0 ){
+         let size = q.length;
+         let cur = [];
+         for(let i = 0;i<size;i++){
+             let node = q.shift();             
+             cur.push(node.val)
+             if(node.left != null) q.push(node.left)
+             if(node.right != null) q.push(node.right)
+         }
+        ans.unshift(cur)
+    }
+   return ans
+};
+
