b951f25454ca118479e5a3dac7ffc43459750750
jk-nishad-72 <jkn18869@gmail.com>
2025-12-02 21:28:22 +0530

    LEFT VIEW DFS DONE

--- Daywise_solutions/DSA_DAY82-BTREE/app.js
+++ Daywise_solutions/DSA_DAY82-BTREE/app.js
@@ -24,3 +24,44 @@ var rightSideView = function(root) {
 
 };
 
+/*
+ *Left View of Binary Tree 
+ * @param {Node} root
+ * @returns {number[]}
+ */
+
+/*
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    leftView(root) {
+        let ans = [] 
+     
+     let solve = function(root , level , ans){
+         if(root == null) return
+         if(level == ans.length) ans.push(root.data)
+         
+         solve(root.left, level+1 , ans)
+         solve(root.right, level+1, ans)
+         
+     }
+     
+     
+     solve(root , 0, ans)
+     return ans 
+     
+    
+     
+    
+     
+    }
+}
+
