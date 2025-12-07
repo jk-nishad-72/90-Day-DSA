d7e8f00e7a0151e62a43cdef3a5fa9ecfcd687aa
jk-nishad-72 <jkn18869@gmail.com>
2025-12-07 08:05:17 +0530

    230 done

--- Daywise_solutions/DSA_DAY87-BST-PROBLEMS/app.js
+++ Daywise_solutions/DSA_DAY87-BST-PROBLEMS/app.js
@@ -1,1 +1,29 @@
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var solve = function(root , ans){
+ 
+     if(root == null) return 
+     solve(root.left , ans)
+     process.stdout.write(root.val +' ')
+     ans.push(root.val ) 
+
+     solve(root.right , ans)
+}
+var kthSmallest = function(root, k) {
+
+    let ans = []
+    solve(root , ans)
+    return ans[k-1]
+};
