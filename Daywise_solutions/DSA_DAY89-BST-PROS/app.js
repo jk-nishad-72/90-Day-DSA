2f56a0585aa3a61bd6b44bc18416a89db097f738
jk-nishad-72 <jkn18869@gmail.com>
2025-12-09 21:13:29 +0530

    530 q. DONE

--- Daywise_solutions/DSA_DAY89-BST-PROS/app.js
+++ Daywise_solutions/DSA_DAY89-BST-PROS/app.js
@@ -28,3 +28,32 @@ var minDiffInBST = function(root) {
    }
     return mind
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**530. Minimum Absolute Difference in BST
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+  var solve = function(root ,  ans ){
+     if(root == null ) return 
+     solve(root.left , ans ) 
+     ans.push(root.val)
+     solve(root.right, ans)
+ }
+var getMinimumDifference = function(root) {
+        let ans = []
+    solve(root , ans)
+  let mind = ans[1]
+   for(let i = 1;i< ans.length;i++ ){
+       mind = Math.min( mind , ans[i] - ans[i-1])
+   }
+    return mind
+};
