a60ea691368bbd315a86619fd725feba1fc1c8ac
jk-nishad-72 <jkn18869@gmail.com>
2025-12-09 21:09:41 +0530

    783 Q done ;

--- Daywise_solutions/DSA_DAY89-BST-PROS/app.js
+++ Daywise_solutions/DSA_DAY89-BST-PROS/app.js
@@ -1,1 +1,30 @@
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**783. Minimum Distance Between BST Nodes
+
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+ var solve = function(root ,  ans ){
+     if(root == null ) return 
+     solve(root.left , ans ) 
+     ans.push(root.val)
+     solve(root.right, ans)
+ }
+var minDiffInBST = function(root) {
+    let ans = []
+    solve(root , ans)
+  let mind = ans[1]
+   for(let i = 1;i< ans.length;i++ ){
+       mind = Math.min( mind , ans[i] - ans[i-1])
+   }
+    return mind
+};
