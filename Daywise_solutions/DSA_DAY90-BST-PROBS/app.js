fae91a940c54ef50ac39d6c5f820988f4852167a
jk-nishad-72 <jkn18869@gmail.com>
2025-12-10 10:06:25 +0530

    99 solved

--- Daywise_solutions/DSA_DAY90-BST-PROBS/app.js
+++ Daywise_solutions/DSA_DAY90-BST-PROBS/app.js
@@ -1,1 +1,28 @@
+var recoverTree = function(root) {
+    let first = null;
+    let second = null;
+    let prev = new TreeNode(-Infinity);
 
+    function inorder(node) {
+        if (!node) return;
+
+        inorder(node.left);
+
+        // Detect violation
+        if (prev.val > node.val) {
+            if (!first) first = prev;
+            second = node;
+        }
+
+        prev = node;
+
+        inorder(node.right);
+    }
+
+    inorder(root);
+
+    // Swap values of the two wrong nodes
+    let temp = first.val;
+    first.val = second.val;
+    second.val = temp;
+};
