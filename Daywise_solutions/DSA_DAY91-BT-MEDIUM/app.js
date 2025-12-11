7902d6116a0e3a752ff624e2edf1989356559a05
jk-nishad-72 <jkn18869@gmail.com>
2025-12-11 20:41:23 +0530

    DSA D91 BT & BST PRPS

--- Daywise_solutions/DSA_DAY91-BT-MEDIUM/app.js
+++ Daywise_solutions/DSA_DAY91-BT-MEDIUM/app.js
@@ -66,3 +66,30 @@ let q = Number(prompt('Enter second node value : '))
 const lca = bst1.lowestCommerAncestor(root , p , q);
 console.log('Lowest Common Ancestor is : ' + lca.val);
 
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**235. Lowest Common Ancestor of a Binary Search Tree
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    
+    if(root === null ) return null
+    if(root.val === p.val || root.val === q.val) return root 
+    let left = lowestCommonAncestor(root.left , p , q ) 
+    let right  = lowestCommonAncestor(root.right , p , q ) 
+    if(left != null && right != null ) return root;
+    else if(left === null || right === null ) return left != null ? left : right;
+    else return null
+
+};
