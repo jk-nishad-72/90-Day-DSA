8e9b2c7cf7a401beeab4d326b5d2ff04610a863d
jk-nishad-72 <jkn18869@gmail.com>
2025-12-16 15:09:07 +0530

    DSA D96 2404 DONE

--- Daywise_solutions/DSA_DAY96-HASHING/app.js
+++ Daywise_solutions/DSA_DAY96-HASHING/app.js
@@ -32,4 +32,38 @@ class Solution {
 let obj = new Solution()
 obj.replaceWithRank(6,[20, 15, 26, 2, 98, 6])
 
+var mostFrequentEven = function(nums) {
+
+    let map = new Map();
+
+    for(let  i = 0;i< nums.length;i++){
+        if(nums[i] % 2 == 0){
+        
+             if(map.has(nums[i])){
+                 map.set(nums[i], map.get(nums[i]) + 1);
+             }else{
+                 map.set(nums[i], 1);
+             }
+    }
+}
+
+    let maxFreq = 0;
+
+  let ans = -1;
+
+for (let [num, freq] of map) {
+  if (
+    freq > maxFreq ||
+    (freq === maxFreq && num < ans)
+  ) {
+    maxFreq = freq;
+    ans = num;
+  }
+}
+
+return ans;
+    
+};
+
+console.log(mostFrequentEven([29,47,21,41,13,37,25,7]));
 
