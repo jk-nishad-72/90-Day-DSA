8cbb5c2a35dd6994f389b8a20109f9c0cad41327
jk-nishad-72 <jkn18869@gmail.com>
2025-12-17 21:35:59 +0530

    389 DONE ;

--- Daywise_solutions/DSA_DAY97-HASHING/app.js
+++ Daywise_solutions/DSA_DAY97-HASHING/app.js
@@ -23,4 +23,50 @@ var findPermutationDifference = function(s, t) {
 };
 
 
-console.log(findPermutationDifference('abc', 'bac'));
+// console.log(findPermutationDifference('abc', 'bac'));
+
+
+/**389. Find the Difference
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+      
+    let s1 = new Map();
+    let t1 = new Map();
+
+    for(let i = 0;i<s.length;i++){
+         if(s1.has(s[i])){
+
+             s1.set(s[i] , s1.get(s[i]) + 1 )
+         }
+         else{
+            s1.set(s[i] ,1 )
+         }
+    }
+
+    for(let i = 0;i<t.length;i++){
+         if(t1.has(t[i])){
+             t1.set(t[i] , t1.get(t[i]) +1 )
+         }else{
+            t1.set(t[i] ,1 )
+         }
+    }
+
+    console.log(s1 , t1);
+    
+ 
+    for(let i = 0;i<t.length;i++){
+
+
+        if(!s1.has(t[i])  || (Math.abs(s1.get(t[i]) - t1.get(t[i])) != 0 )){
+
+            return t[i];
+     }
+
+} 
+
+};
+
+console.log(findTheDifference('abcd','abcde'));
