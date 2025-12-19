2fa8b4056b8f537a11dc9df4a4e3f9ae476c9310
jk-nishad-72 <jkn18869@gmail.com>
2025-12-19 21:21:34 +0530

    DSA D99  2062. Count Vowel Substrings of a String ✅ DONE

--- Daywise_solutions/DSA_DAY99-HASHING/app.js
+++ Daywise_solutions/DSA_DAY99-HASHING/app.js
@@ -54,3 +54,29 @@ var uniqueMorseRepresentations = function(words) {
 };
 
 uniqueMorseRepresentations(["a"])
+
+/**2062. Count Vowel Substrings of a String
+ * @param {string} word
+ * @return {number}
+ */
+var countVowelSubstrings = function(word) {
+
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        let seen = new Set();
+
+        for (let j = i; j < word.length; j++) {
+            if (!vowels.has(word[j])) break;
+
+            seen.add(word[j]);
+
+            if (seen.size === 5) {
+                count++;
+            }
+        }
+    }
+
+    return count;  
+};
