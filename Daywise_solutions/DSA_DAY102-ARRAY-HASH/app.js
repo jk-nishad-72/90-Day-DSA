d026bb9fcc61f2b9106066138c690ffecb9e7130
jk-nishad-72 <jkn18869@gmail.com>
2025-12-22 22:56:07 +0530

    DSA D102 ARRAY AND HASHING DONE

--- Daywise_solutions/DSA_DAY102-ARRAY-HASH/app.js
+++ Daywise_solutions/DSA_DAY102-ARRAY-HASH/app.js
@@ -1,7 +1,24 @@
+/**3731. Find Missing Elements
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 
 var findMissingElements = function(nums) {
-    
+     let set = new Set();
+     nums.sort((a,b)=> a-b)
 
+     let  min = nums[0]
+     let max = nums[nums.length-1]
+       for(let i = 0;i<nums.length ; i++){
 
-    
+              set.add(nums[i])
+       }
+       let ans = [];
+       for(let i = min;i<= max ;i++){
+             if(!set.has(i)) ans.push(i)
+       }
+    return ans
+       
 };
+
+console.log(findMissingElements([5,1]));
