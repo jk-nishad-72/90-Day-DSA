f20b8da9de94731e65d18fefe58b7904d2bce3cf
jk-nishad-72 <jkn18869@gmail.com>
2025-12-24 22:52:24 +0530

    DSA D104 STRING AND HASHING

---
+++ Daywise_solutions/DSA_DAY-104-STRING-HASH/app.js
@@ -0,0 +1,13 @@
+
+
+/**3. Longest Substring Without Repeating Characters
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+     const set = new Set(s);
+
+     return set.size
+     
+};
