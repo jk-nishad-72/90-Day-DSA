7a396f615e71befe7ed40aec7cf23d8ead7cda0b
jk-nishad-72 <jkn18869@gmail.com>
2025-12-26 23:50:10 +0530

    DSA DAY 106 ARRAY STRING

--- Daywise_solutions/DSA_DAY106-ARRAY-STRING/app.js
+++ Daywise_solutions/DSA_DAY106-ARRAY-STRING/app.js
@@ -1,2 +1,19 @@
+var removeAnagrams = function(words) {
+    let result = [];
+    
+    for (let word of words) {
+        let sorted = word.split('').sort().join('');
+        
+        if (
+            result.length === 0 ||
+            sorted !== result[result.length - 1].split('').sort().join('')
+        ) {
+            result.push(word);
+        }
+    }
+    
+    return result;
+};
 
-console.log('hello')
+
+console.log(removeAnagrams(["abba","baba","bbaa","cd","cd"]));
