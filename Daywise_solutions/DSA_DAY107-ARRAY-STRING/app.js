c641fb0ee5454fc7e3338efb1f7f6a2fbf2006fb
jk-nishad-72 <jkn18869@gmail.com>
2025-12-27 22:40:07 +0530

    DSA D107 DONE

--- Daywise_solutions/DSA_DAY107-ARRAY-STRING/app.js
+++ Daywise_solutions/DSA_DAY107-ARRAY-STRING/app.js
@@ -1,4 +1,31 @@
 
 
-console.log('hello')
+/**49. Group Anagrams
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs){
 
+     let map = new  Map();
+
+     for(let word of strs){
+         let sorted = word.split('').sort().join('') 
+
+        if(map.has(sorted)){
+            map.set(sorted,[...map.get(sorted),word])
+        }else{
+             map.set(sorted, [word])
+        }
+         
+     }
+    let result = [];
+
+    for(let [key , values] of map){
+         result.push(values)
+         
+    }
+    return result
+};
+
+
+console.log(groupAnagrams( ["eat","tea","tan","ate","nat","bat"]));
