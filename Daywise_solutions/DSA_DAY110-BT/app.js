47ee22b1af64e3ee6fd39b7ddd3842b0d0098672
jk-nishad-72 <jkn18869@gmail.com>
2025-12-30 23:51:14 +0530

    DSA D110 DONE

--- Daywise_solutions/DSA_DAY110-BT/app.js
+++ Daywise_solutions/DSA_DAY110-BT/app.js
@@ -1,2 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**129. Sum Root to Leaf Numbers
+ * @param {TreeNode} root
+ * @return {number}
+ */
 
-console.log('halloo ');
+ var solve = function(root , currentNum){
+      if(root === null ) return 0;
+      currentNum = currentNum * 10 + root.val;
+      if(root.left === null && root.right === null ) return currentNum;
+      let left = solve(root.left, currentNum)
+      let right = solve(root.right, currentNum)
+      return left + right
+ }
+var sumNumbers = function(root) {
+    return solve(root, 0)    
+};
