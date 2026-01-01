63b056376a93748bf917f46f72c720ecae49a7b1
jk-nishad-72 <jkn18869@gmail.com>
2026-01-01 23:27:02 +0530

    DSA DAY 112 DONE 653. Q SOLVED 653. Two Sum IV - Input is a BST

--- Daywise_solutions/DSA_DAY112-BST/app.js
+++ Daywise_solutions/DSA_DAY112-BST/app.js
@@ -1,3 +1,44 @@
 
 
 
+/*
+ *653. Two Sum IV - Input is a BST .✅🎉
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    let nums = [];
+    function inorder(root){
+         if(root === null ) return ;
+         nums.push(root.val)
+         inorder(root.left)
+        //  console.log(root.val)
+         inorder(root.right)
+    }
+ 
+    inorder(root)
+    // console.log(nums)
+   return twoSum(nums, k);
+};
+
+var twoSum = function(nums, target) {
+       let map = new Map();
+  
+    for(let i =0;i<nums.length;i++){
+        if(map.has(target-nums[i])){
+            return true
+        }else{
+            map.set(nums[i],i)
+        }
+    }
+    return false
+};
