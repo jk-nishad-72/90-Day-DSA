c4b572595dafd8dbbc0a148c4894667fdf8ac71e
jk-nishad-72 <jkn18869@gmail.com>
2026-01-02 23:52:18 +0530

    DSA DAY 113 LC Q. 66 PLUSH ONE DONE

--- Daywise_solutions/DSA_DAY113-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY113-ARRAY/app.js
@@ -1,3 +1,22 @@
+/**66. Plus One
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) { 
+    
+  for(let i = digits.length-1;i >= 0;i--){
+       
+        digits[i] += 1;
 
+        if(digits[i]<10){
+            return digits
+        }
 
-console.log('hallo ');
+        digits[i] = 0;
+
+  }
+
+  digits.unshift(1)
+  return digits
+
+};
