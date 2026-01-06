2b3b55f33a6bec83990feb957b46f30223d9d7a9
jk-nishad-72 <jkn18869@gmail.com>
2026-01-06 20:36:25 +0530

    done  d117 LC 347 . done

--- Daywise_solutions/DSA_DAY117-HASHING/app.js
+++ Daywise_solutions/DSA_DAY117-HASHING/app.js
@@ -1,2 +1,38 @@
 
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+let map = new Map();
+
+if(nums.length === 1) return nums
+nums.sort((a,b) => a - b)
+for(let i = 0;i<nums.length;i++){
+      if(map.has(nums[i])){
+        map.set(nums[i] , map.get(nums[i]) + 1)
+      }else{
+        map.set(nums[i] , 1)
+      }
+}
+
+// console.log(map);
+
+ let ans = [];
+    let sorted = Array.from(map).sort((a,b) => b[1] - a[1]);
+    for(let i = 0;i<k;i++){
+        ans.push(sorted[i][0])
+    }   
+
+  
+
+  console.log( sorted, ans);
+  
+return ans
+
+};
+
+
+console.log(topKFrequent( [3,0,1,0]) , 1) 
