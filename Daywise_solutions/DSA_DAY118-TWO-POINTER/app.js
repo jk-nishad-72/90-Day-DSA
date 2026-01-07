aded01f9432624f38e4549078dcfad2c25897fd2
jk-nishad-72 <jkn18869@gmail.com>
2026-01-07 23:17:01 +0530

    DONE *844. Backspace String Compare

--- Daywise_solutions/DSA_DAY118-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY118-TWO-POINTER/app.js
@@ -1,2 +1,54 @@
 
 
+/**844. Backspace String Compare
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    let i = s.length - 1;
+    let j = t.length - 1;
+
+    let skipS = 0;
+    let skipT = 0;
+
+    while (i >= 0 || j >= 0) {
+
+        // Find next valid character in s
+        while (i >= 0) {
+            if (s[i] === '#') {
+                skipS++;
+                i--;
+            } else if (skipS > 0) {
+                skipS--;
+                i--;
+            } else {
+                break;
+            }
+        }
+
+        // Find next valid character in t
+        while (j >= 0) {
+            if (t[j] === '#') {
+                skipT++;
+                j--;
+            } else if (skipT > 0) {
+                skipT--;
+                j--;
+            } else {
+                break;
+            }
+        }
+        // Compare characters
+        let charS = i >= 0 ? s[i] : null;
+        let charT = j >= 0 ? t[j] : null;
+        if (charS !== charT) return false;
+        i--;
+        j--;
+    }
+
+    return true;
+};
+
+
+
