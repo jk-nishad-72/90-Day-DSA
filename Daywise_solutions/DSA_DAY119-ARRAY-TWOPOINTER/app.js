f8120a788c785a4e2f6fd8fbdce44921fe581fcb
jk-nishad-72 <jkn18869@gmail.com>
2026-01-08 23:23:51 +0530

    SOLVED 189 . ROTATE ARRAY

--- Daywise_solutions/DSA_DAY119-ARRAY-TWOPOINTER/app.js
+++ Daywise_solutions/DSA_DAY119-ARRAY-TWOPOINTER/app.js
@@ -1,2 +1,31 @@
 
 
+/**189. Rotate Array
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+ var reverse = function(arr, i , j){
+
+     while(i<j){
+          let temp = arr[i];
+          arr[i]  = arr[j];
+          arr[j] = temp;
+          i++
+          j--
+     }
+ }
+var rotate = function(nums, k) {  
+
+    k = k % nums.length;
+    reverse(nums, 0 , nums.length-1);
+    reverse(nums, k , nums.length-1);
+    reverse(nums, 0 ,k-1 );
+
+    return nums;
+};
+
+
+console.log(rotate([-1], k = 3));
+
