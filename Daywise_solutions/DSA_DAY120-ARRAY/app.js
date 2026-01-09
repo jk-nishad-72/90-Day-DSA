1cee5311d162767f2931774aaeb93fd2f0c63fa1
jk-nishad-72 <jkn18869@gmail.com>
2026-01-09 23:19:23 +0530

    724. Find Pivot Index SOLVED

--- Daywise_solutions/DSA_DAY120-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY120-ARRAY/app.js
@@ -1,5 +1,28 @@
 
+/**724. Find Pivot Index
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+ 
+    const totalSum = nums.reduce((acc, num) => acc + num, 0);
 
-console.log('helloo ')
+    let leftSum = 0;
+    let rightSum = totalSum;
 
+    for(let i = 0;i<nums.length;i++){
+
+         rightSum -= nums[i];
+
+         if(leftSum === rightSum){
+            return i;
+         }else{
+            leftSum += nums[i];
+         }
+    }
+    return -1;
+    
+};
+
+console.log(pivotIndex([2 , 1, -1]));
 
