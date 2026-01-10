47110ac33a66b08b6444731fe1b3be1fda020ee2
jk-nishad-72 <jkn18869@gmail.com>
2026-01-10 21:51:44 +0530

    DSA DAY 121 ARRAY BIT MANIPULATION

---
+++ Daywise_solutions/DSA_DAY121-ARRAY/app.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    
+    // let map = new Map();
+
+    // for(let  i = 0;i<nums.length;i++){
+
+    //      if(map.has(nums[i])){
+    //          map.set(nums[i], map.get(nums[i]) + 1);
+    //      }else{
+    //          map.set(nums[i],1);
+    //      }
+    // }
+    
+    // for (let [key, value] of map) {
+    //     if(value === 1){
+    //         return key;
+    //     }}
+    // console.log(map)
+    
+};
+
+console.log(singleNumber( nums = [2,2,3,2]));
+
+
