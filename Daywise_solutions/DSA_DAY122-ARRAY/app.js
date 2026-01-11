6d66d83b77a6ea91c363e2a1648874bbc83ed49d
jk-nishad-72 <jkn18869@gmail.com>
2026-01-11 21:32:02 +0530

    DSA DAY 122 SOLVED 1991. Find the Middle Index in Array

--- Daywise_solutions/DSA_DAY122-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY122-ARRAY/app.js
@@ -1,2 +1,21 @@
 
 
+/*
+ *1991. Find the Middle Index in Array
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function(nums) {
+    const totalSum = nums.reduce((acc, num ) => acc + num , 0)
+    let leftSum = 0;
+    let rightSum = totalSum ;
+    for(let  i = 0;i<nums.length;i++){
+          rightSum -= nums[i];
+          if(leftSum === rightSum ){        
+              return i
+          }else {
+            leftSum += nums[i]
+          }
+    }
+ return -1;
+};
--- Daywise_solutions/DSA_DAY122-ARRAY/script.py
+++ Daywise_solutions/DSA_DAY122-ARRAY/script.py
@@ -1,1 +1,21 @@
 
+class Solution(object):
+    def findMiddleIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total_sum = sum(nums)
+        print("Total sum of array:", total_sum)
+        left_sum = 0
+        right_sum = total_sum
+        for i in range(len(nums)):
+            right_sum -= nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+        return -1   
+# Example usage:
+solution = Solution()
+print(solution.findMiddleIndex([2, 3, -1, 8, 4]))  # Output: 3
+print(solution.findMiddleIndex([1, -1, 4]))         # Output:
