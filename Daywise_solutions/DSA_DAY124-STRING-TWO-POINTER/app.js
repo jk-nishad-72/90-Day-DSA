007ffaa06615c54361db7f44c4d724513b8355c1
jk-nishad-72 <jkn18869@gmail.com>
2026-01-13 20:54:13 +0530

    DSA DAY 124 DONE ## DAY_124 STRING TWO POINTER ➡️ 13/01/2026
    —Q. [**28. Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/) ✅**## DAY_124 STRING TWO POINTER ➡️ 13/01/2026
    
    —Q. [**28. Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/) ✅**

--- Daywise_solutions/DSA_DAY124-STRING-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY124-STRING-TWO-POINTER/app.js
@@ -1,3 +1,16 @@
+/*
+*28. Find the Index of the First Occurrence in a String
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    if(haystack.includes(needle)){
+        return haystack.indexOf(needle);
+    }
+    return -1;
+}
 
+console.log(strStr( "leetcode", needle = "leeto"));
 
-console.log('Hello, World!');
