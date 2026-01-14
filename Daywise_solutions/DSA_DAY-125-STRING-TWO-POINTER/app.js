0bb8d3445269c2117b249bb7304358ac58ee0b8d
jk-nishad-72 <jkn18869@gmail.com>
2026-01-14 22:28:58 +0530

    DSA DAY 125 SOLVED

--- Daywise_solutions/DSA_DAY-125-STRING-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY-125-STRING-TWO-POINTER/app.js
@@ -1,5 +1,26 @@
+/**151. Reverse Words in a String
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
 
 
-console.log('DSA DAY 125 STRING - TWO POINTER ')
+    //  let words = s.split(" ");
+    //  console.log(words);
 
+    //  let ans = ''
+    //  for(let i = words.length-1;i>=0;i--){
+    //         if(words[i] !== ''){
+    //             ans += words[i] + ' ';
+    //         }
+         
+    //  }
+    //     ans = ans.trim();
+    // return ans;
 
+    let words = s.trim().split(/\s+/);
+    console.log(words);
+    return words.reverse().join(' ');
+
+};
+console.log(reverseWords("a good   example"));
