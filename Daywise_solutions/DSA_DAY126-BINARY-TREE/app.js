7dd467e77b9138ad96e98e1a766791a514365313
jk-nishad-72 <jkn18869@gmail.com>
2026-01-15 23:03:25 +0530

    DSA DAY 126 DONE

--- Daywise_solutions/DSA_DAY126-BINARY-TREE/app.js
+++ Daywise_solutions/DSA_DAY126-BINARY-TREE/app.js
@@ -1,2 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**111. Minimum Depth of Binary Tree
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if(root == null ) return 0;
+    if(root.left === null && root.right === null ) return 1;
+    if(root.left == null ) return minDepth(root.right)+1;
+    if(root.right == null ) return minDepth(root.left)+1;
 
-console.log('DAY_126 BINARY TREE ➡️ 15/01/2026');
+    let left = minDepth(root.left)
+    let right = minDepth(root.right)
+
+    return Math.min(left , right) +1
+
+};
