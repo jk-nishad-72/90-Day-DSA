1956a527559f6a905638647d9bc7f4f9a876c272
jk-nishad-72 <jkn18869@gmail.com>
2026-01-18 21:58:46 +0530

    DSA DAY 129 DONE

--- Daywise_solutions/DSA_DAY129-STRING/app.js
+++ Daywise_solutions/DSA_DAY129-STRING/app.js
@@ -1,3 +1,18 @@
+/**58. Length of Last Word
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    
+
+     let Words = s.trim().split(" ");
+     return Words[Words.length -1].length;
+ 
+  
+
+   
 
 
-console.log('Hello, World!');
+};
+
+console.log(lengthOfLastWord("Hello World"));
