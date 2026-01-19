40e18c84b7b06088c3fdbc3ce2f4bd58208402c3
jk-nishad-72 <jkn18869@gmail.com>
2026-01-19 22:27:53 +0530

    DSA DAY 130 344. Reverse String SOLVED

--- Daywise_solutions/DSA_DAY130-STRING-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY130-STRING-TWO-POINTER/app.js
@@ -1,5 +1,21 @@
+/**344. Reverse String
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    
+    let i = 0;
+    let j = s.length-1;
+    while(i<j){
 
+         let temp = s[i]
+         s[i]  = s[j]
+         s[j] = temp
+         j--
+         i++
+    }
+    return s
+};
 
-console.log('DSA DAY 130 STRING TWO POINTER ');
-
+console.log(reverseString(["h","e","l","l","o"]));
 
