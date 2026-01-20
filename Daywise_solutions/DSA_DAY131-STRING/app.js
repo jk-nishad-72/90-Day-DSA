d4effb82da85a6833da57fbfea15c7f8b785780b
jk-nishad-72 <jkn18869@gmail.com>
2026-01-20 22:14:43 +0530

    DSA DAY 131 DONE

--- Daywise_solutions/DSA_DAY131-STRING/app.js
+++ Daywise_solutions/DSA_DAY131-STRING/app.js
@@ -1,2 +1,30 @@
 
 
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    const chars = s.split('');
+    let left = 0;
+    let right = chars.length - 1;
+    while (left < right) {
+        while (left < right && !vowels.has(chars[left])) {
+            left++;
+        }   
+        while (left < right && !vowels.has(chars[right])) {
+            right--;
+        }
+        if (left < right) {
+            [chars[left], chars[right]] = [chars[right], chars[left]];
+            left++;
+            right--;
+        }
+    }
+    return chars.join('');
+};
+
+console.log(reverseVowels("IceCreAm"));
