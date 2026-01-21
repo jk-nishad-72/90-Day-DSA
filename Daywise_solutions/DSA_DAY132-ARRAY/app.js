9e0c9887f9d1fc8f80a622bcc9094cc4ef4319af
jk-nishad-72 <jkn18869@gmail.com>
2026-01-21 22:10:54 +0530

    DSA D132 74. Search a 2D Matrix  SOLVED

--- Daywise_solutions/DSA_DAY132-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY132-ARRAY/app.js
@@ -1,5 +1,19 @@
 
+/**74. Search a 2D Matrix
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    
+    for(let i = 0;i<matrix.length;i++){
 
+         for(let j = 0;j<matrix[i].length;j++){
 
-console.log("Hello, World!");
+                if(matrix[i][j] === target) return true
+         }
+    }
+    return false
+};
 
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3));
