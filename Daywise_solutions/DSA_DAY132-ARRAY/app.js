e2086f0c9b6b95f082d9f38bea014ca46c12c9e6
jk-nishad-72 <jkn18869@gmail.com>
2026-01-21 22:25:18 +0530

    DSA  DAY 132 TOPICS ADDED

--- Daywise_solutions/DSA_DAY132-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY132-ARRAY/app.js
@@ -4,7 +4,7 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
+var searchMatrix = function(matrix, target) { 
     
     for(let i = 0;i<matrix.length;i++){
 
@@ -16,4 +16,4 @@ var searchMatrix = function(matrix, target) {
     return false
 };
 
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3));
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3)); 
