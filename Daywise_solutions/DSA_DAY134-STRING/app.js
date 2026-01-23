f381c336e8533329aa20957ea1fa4b2cc50b9a6e
jk-nishad-72 <jkn18869@gmail.com>
2026-01-23 23:07:19 +0530

    D134 DONE

--- Daywise_solutions/DSA_DAY134-STRING/app.js
+++ Daywise_solutions/DSA_DAY134-STRING/app.js
@@ -1,2 +1,25 @@
 
 
+/**541. Reverse String II
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+
+    let arr = s.split('');
+    for(let i = 0; i < arr.length; i += 2 * k) {
+        let left = i;
+        let right = Math.min(i + k - 1, arr.length - 1);
+
+        while(left < right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+
+    return arr.join('');    
+};
+
+console.log(reverseStr( s = "abcdefg", k = 2));
