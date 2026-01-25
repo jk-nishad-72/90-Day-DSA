13a4ae1eb39c9eea46673f67d54ccaf2f27aa814
jk-nishad-72 <jkn18869@gmail.com>
2026-01-25 16:59:29 +0530

    D136 DONE

--- Daywise_solutions/DSA_DAY136-STRING/app.js
+++ Daywise_solutions/DSA_DAY136-STRING/app.js
@@ -1,4 +1,38 @@
 
-console.log('DSA DAY 136-STRING ');
+
+/*
+ *2810. Faulty Keyboard
+ * @param {string} s
+ * @return {string}
+ */
+
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }   
+    return s
+}
+
+var finalString = function(s) {
+    
+    let result = [];
+    for (let char of s) {
+        if (char === "i") {
+            result = reverseString(result);
+        } else {
+            result.push(char);
+        }   
+    }
+    return result.join(''); 
+      
+};
 
 
+console.log(finalString("string"));
