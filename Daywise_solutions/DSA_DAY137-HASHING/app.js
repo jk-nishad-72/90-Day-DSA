00058f6242033cbda7379c3ac8a6abb72991e644
jk-nishad-72 <jkn18869@gmail.com>
2026-01-26 16:27:09 +0530

    DSA DAY 137 DONE

--- Daywise_solutions/DSA_DAY137-HASHING/app.js
+++ Daywise_solutions/DSA_DAY137-HASHING/app.js
@@ -1,4 +1,36 @@
 
+/*
+ *500. Keyboard Row
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
 
-console.log(' DSA DAY 137 HASHING ')
+  let s = "qwertyuiop"
+  let d = "asdfghjkl"
+  let f = "zxcvbnm"
+    let res = []
+    for(let word of words){
+        let lowerWord = word.toLowerCase()
+        let row1 = true
+        let row2 = true
+        let row3 = true
+        for(let char of lowerWord){
+            if(!s.includes(char)){
+                row1 = false
+            }
+            if(!d.includes(char)){
+                row2 = false
+            }
+            if(!f.includes(char)){
+                row3 = false
+            }
+        }
+        if(row1 || row2 || row3){
+            res.push(word)
+        }
+    }
+    return res
+};
 
+console.log(findWords(["Hello","Alaska","Dad","Peace"]));
