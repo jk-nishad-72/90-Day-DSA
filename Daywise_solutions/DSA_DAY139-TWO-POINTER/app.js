b868219040d3e999c5f86a1f678110368b67e022
jk-nishad-72 <jkn18869@gmail.com>
2026-01-28 23:16:34 +0530

    D139 DONE

--- Daywise_solutions/DSA_DAY139-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY139-TWO-POINTER/app.js
@@ -1,5 +1,37 @@
 
+/*
+*917. Reverse Only Letters
+ * @param {string} s
+ * @return {string}
+ */
+var reverseOnlyLetters = function(s) {
+  
+    const set = new Set();
+    for( let i = 0; i < 26; i++ ){
+      set.add( String.fromCharCode( 'a'.charCodeAt(0) + i ) );
+      set.add( String.fromCharCode( 'A'.charCodeAt(0) + i ) );
+    }
 
-console.log('DSA DAY 139 TWO POINTER ');
+    
+    
+    const arr = s.split('');
+    let left = 0;
+    let right = arr.length - 1; 
+    while( left < right ){
+      while( left < right && !set.has( arr[left] ) ){
+        left++;
+      }
+      while( left < right && !set.has( arr[right] ) ){
+        right--;
+      }
+      if( left < right ){
+        [ arr[left], arr[right] ] = [ arr[right], arr[left] ];
+        left++;
+        right--;
+      }
+    }
+    return arr.join('');
+  }
 
-
+  console.log( reverseOnlyLetters("ab-cd") ); // "dc-ba"
+  
