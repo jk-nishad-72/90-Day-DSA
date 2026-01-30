fe7ada0e36352af00872a3c3116f09d4b9640f22
jk-nishad-72 <jkn18869@gmail.com>
2026-01-30 14:08:02 +0530

    DSA DAY 141 DONE

--- Daywise_solutions/DSA_DAY141-HASHING/app.js
+++ Daywise_solutions/DSA_DAY141-HASHING/app.js
@@ -1,8 +1,35 @@
+/*
+ *859. Buddy Strings
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+    if (s.length !== goal.length) return false;
 
-console.log(' DSA DAY 141 HASHING ');
+    if (s === goal) {   
+        const charSet = new Set();
+        for (let char of s) {
+            if (charSet.has(char)) {
+                return true; 
+            }
+            charSet.add(char);
+        }   
+
+
+        return false;    } else {
+        const diff = [];
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] !== goal[i]) {
+                diff.push(i);
+            }
+        }
+        return diff.length === 2 && s[diff[0]] === goal[diff[1]] && s[diff[1]] === goal[diff[0]];
+    }   
 
 
 
+    
+};
 
-
-
+console.log(buddyStrings(s = "ab", goal = "ba"));
