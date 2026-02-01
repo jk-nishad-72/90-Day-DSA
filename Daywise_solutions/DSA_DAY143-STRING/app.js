f05049cf3423be68669bb2b5437b0df22edc20f6
jk-nishad-72 <jkn18869@gmail.com>
2026-02-01 23:23:09 +0530

    D143 DONE

--- Daywise_solutions/DSA_DAY143-STRING/app.js
+++ Daywise_solutions/DSA_DAY143-STRING/app.js
@@ -1,3 +1,20 @@
+/*
+ *520. Detect Capital
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    
+    const isAllUpper = word === word.toUpperCase();
+    const isAllLower = word === word.toLowerCase();
+    const isFirstUpperRestLower = word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase();
 
+    return isAllUpper || isAllLower || isFirstUpperRestLower;
+    
+};
 
-console.log(' DSA DAY 143 STRING ');
+// Example usage:
+console.log(detectCapitalUse("USA")); // true
+console.log(detectCapitalUse("leetcode")); // true
+console.log(detectCapitalUse("Google")); // true
+console.log(detectCapitalUse("FlaG")); // false
