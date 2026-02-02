317ec75ada0723ec0ec987129fb5783af4bdb6c8
jk-nishad-72 <jkn18869@gmail.com>
2026-02-02 21:04:19 +0530

    DSA 144 DONE

--- Daywise_solutions/DSA_DAY144-HASHING/app.js
+++ Daywise_solutions/DSA_DAY144-HASHING/app.js
@@ -1,3 +1,28 @@
+/*
+ *451. Sort Characters By Frequency
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
 
+    const freqMap = new Map();
 
-console.log(' DSA DAY 144 HASHING ');
+    // Count frequency of each character
+    for( char of s ) {
+        freqMap.set(char , (freqMap.get(char) || 0) +1 );
+    }
+
+    // Convert map to array and sort by frequency
+    let arr = []
+    for( let [char, freq] of freqMap ) {
+        // console.log(char, freq);
+        arr.push( [char, freq] );
+    }
+    arr.sort( (a,b) => b[1] - a[1] );
+
+    // Build the result string
+    return arr.map( e => e[0].repeat(e[1]) ).join('');
+
+};
+
+console.log(frequencySort("cccaaa"));
