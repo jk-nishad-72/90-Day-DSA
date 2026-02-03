3e9c1416b4740cd9499ab1929ba673fa7dbc8958
jk-nishad-72 <jkn18869@gmail.com>
2026-02-03 20:31:28 +0530

    d145 done

--- Daywise_solutions/DSA_DAY145-HASHING/app.js
+++ Daywise_solutions/DSA_DAY145-HASHING/app.js
@@ -1,3 +1,23 @@
+/*
+ *1394. Find Lucky Integer in an Array
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findLucky = function(arr) {
+    
+    const frequencyMap = new Map();
 
+    for (const num of arr) {
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
+    }
+let ans = -1
+    for (const [num, freq] of frequencyMap.entries()) {
+        if (num === freq) {
+            ans = Math.max(ans, num);
+        }
+    }
+    return ans;
+    
+};
 
-console.log(' DSA DAY 145 HASHING ');
+console.log(findLucky([2,2,2,3,3]));
