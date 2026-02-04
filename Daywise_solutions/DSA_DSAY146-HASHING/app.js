dbe39c0643b3356e4c6a8e433fdca0f2204688fc
jk-nishad-72 <jkn18869@gmail.com>
2026-02-04 21:34:40 +0530

    D146 DONE

--- Daywise_solutions/DSA_DSAY146-HASHING/app.js
+++ Daywise_solutions/DSA_DSAY146-HASHING/app.js
@@ -1,3 +1,26 @@
+/*
+ *383. Ransom Note
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
 
-console.log('Hello, World!');
 
+    let map = new Map();
+
+
+    for (let char of magazine) {
+        map.set(char, (map.get(char) || 0) + 1);
+    }
+    for (let char of ransomNote) {
+        if (!map.has(char) || map.get(char) === 0) {
+            return false;
+        }
+        map.set(char, map.get(char) - 1);
+    }
+    return true;
+};
+
+console.log(canConstruct(ransomNote =
+    "aab", magazine = "baa"));
