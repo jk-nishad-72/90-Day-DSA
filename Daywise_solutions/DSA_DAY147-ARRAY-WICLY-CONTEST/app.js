d27954f1a658c72cbd1a8f25431300d092c35cb4
jk-nishad-72 <jkn18869@gmail.com>
2026-02-05 22:14:18 +0530

    D147 DONE

--- Daywise_solutions/DSA_DAY147-ARRAY-WICLY-CONTEST/app.js
+++ Daywise_solutions/DSA_DAY147-ARRAY-WICLY-CONTEST/app.js
@@ -1,3 +1,17 @@
+/*
+*3379. Transformed Array
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var constructTransformedArray = function(nums) {
+  let n = nums.length;
+    let result = new Array(n);
 
+    for (let i = 0; i < n; i++) {
+        let newIndex = ((i + nums[i]) % n + n) % n;
+        result[i] = nums[newIndex];
+    }
 
-console.log('DSA DAY 147 ARRAY WICKLY CONTEST');
+    return result;
+
+};
