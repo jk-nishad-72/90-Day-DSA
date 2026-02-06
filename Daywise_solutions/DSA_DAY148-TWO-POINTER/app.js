ce1a8636d82d2b31b337f1655b87952d8308ffa6
jk-nishad-72 <jkn18869@gmail.com>
2026-02-06 22:02:53 +0530

    DONE

--- Daywise_solutions/DSA_DAY148-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY148-TWO-POINTER/app.js
@@ -1,4 +1,24 @@
+/*
+ *27. Remove Element
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+    let i = 0;
+    let j = 0;
+    while(j < nums.length){
 
+          if(nums[j] !== val){
+               let temp = nums[i];
+               nums[i] = nums[j];
+               nums[j] = temp;
+               i++;
+          }
+          j++;
+    }
+    return i;
+};
 
-console.log('DSA DAY 148 TWO POINTER ');
-
+console.log(removeElement( nums = [3,2,2,3], val = 3)) // 2;
