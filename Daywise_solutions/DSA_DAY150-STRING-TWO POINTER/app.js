91ac9cef886605002a987cccb0f2851cd6b4809d
jk-nishad-72 <jkn18869@gmail.com>
2026-02-08 22:41:17 +0530

    DSA D150 DONE

--- Daywise_solutions/DSA_DAY150-STRING-TWO POINTER/app.js
+++ Daywise_solutions/DSA_DAY150-STRING-TWO POINTER/app.js
@@ -1,4 +1,25 @@
+/*
+ *1332. Remove Palindromic Subsequences
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
 
+    if(s.length === 0) return 0;
 
+    let left = 0;
+    let right = s.length - 1;
 
-console.log('DAY_150 STRING - TWO POINTER  ➡️ 08022026');
+    while(left < right) {
+        if(s[left] !== s[right]) {
+            return 2;
+        }
+        left++;
+        right--;
+    }
+    return 1;
+
+    
+};
+
+console.log(removePalindromeSub("baabb"));
