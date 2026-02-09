001192d44a5c44951955c76197d934e75d94761c
jk-nishad-72 <jkn18869@gmail.com>
2026-02-09 22:35:20 +0530

    DONE

--- Daywise_solutions/DSA_DAY151-BST/app.js
+++ Daywise_solutions/DSA_DAY151-BST/app.js
@@ -1,3 +1,34 @@
+/*
+ *108. Convert Sorted Array to Binary Search Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
 
 
-console.log('DAY_151 BST ➡️ 09/02/2026');
+var sortedArrayToBST = function(nums) {
+    
+    if(nums.length === 0) return null;
+    
+    let mid = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[mid]);
+    
+    root.left = sortedArrayToBST(nums.slice(0, mid));
+    root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+    return root;    
+
+
+    };
+
+    console.log(sortedArrayToBST([-10,-3,0,5,9]));
+
+
+    
