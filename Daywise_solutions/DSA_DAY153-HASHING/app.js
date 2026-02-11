3dab9d14c83aa134c06baa2835f4cbf5cd831812
jk-nishad-72 <jkn18869@gmail.com>
2026-02-11 22:37:25 +0530

    DONE

--- Daywise_solutions/DSA_DAY153-HASHING/app.js
+++ Daywise_solutions/DSA_DAY153-HASHING/app.js
@@ -1,3 +1,23 @@
+/*
+ *961. N-Repeated Element in Size 2N Array
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            return nums[i];
+        }else{
+            map.set(nums[i], 1);
+        }
+    }
 
-console.log('DAY_153 HASHING ➡️ 11022026');
 
+
+
+};
+
+console.log(repeatedNTimes([2,1,2,5,3,2]));
+// console.log(repeatedNTimes([1,2,3,3]));
