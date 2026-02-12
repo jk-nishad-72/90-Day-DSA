756b21d1208f320d9bb5539b17cc066a1470b8b3
jk-nishad-72 <jkn18869@gmail.com>
2026-02-12 22:50:34 +0530

    done

--- Daywise_solutions/DSA_DAY154-HASHING/app.js
+++ Daywise_solutions/DSA_DAY154-HASHING/app.js
@@ -1,2 +1,27 @@
+/*
+ *1748. Sum of Unique Elements
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function(nums) {
+   
+    let map = new Map();
 
-console.log('DAY_154 HASHING ➡️ 12/02/2026');
+    for(let num of nums){
+         map.set(num, (map.get(num) || 0) + 1)
+    }
+
+     let sum  = 0;
+    for(let [key ,value] of map){
+             if(value === 1 ) sum += key
+         
+    }
+    
+    return sum
+
+};
+
+// console.log(sumOfUnique([1,1,1,1,1]));
+console.log(sumOfUnique([1,2,3,4,5]));
+
+// console.log(sumOfUnique([1,2,3,2]));
