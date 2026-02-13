3ec76ef98a1f60cf2a9761f59468fd482d4273df
jk-nishad-72 <jkn18869@gmail.com>
2026-02-13 22:49:28 +0530

    done with recursion

--- Daywise_solutions/DSA_DAY155-MATH/app.js
+++ Daywise_solutions/DSA_DAY155-MATH/app.js
@@ -1,2 +1,17 @@
+/*
+ *231. Power of Two
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    
+         if (n === 1) return true; 
+         if  (n<1 || n % 2 !== 0) return false;
+         return  isPowerOfTwo(n /2)
+};
 
-console.log('DAY_155 MATH ➡️ 13/02/2026');
+console.log(isPowerOfTwo(3));
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(4));
+console.log(isPowerOfTwo(18));
