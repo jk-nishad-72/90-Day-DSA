31675d94c8239671d4b5d6b57645838e9478e610
jk-nishad-72 <jkn18869@gmail.com>
2026-02-15 22:55:20 +0530

    done

--- Daywise_solutions/DSA_DAY157-HASHING/app.js
+++ Daywise_solutions/DSA_DAY157-HASHING/app.js
@@ -1,3 +1,25 @@
+/**1796. Second Largest Digit in a String
+ * @param {string} s
+ * @return {number}
+ */
+var secondHighest = function(s) {
+    
+    let max = -1;
+    let scMax = -1;
+    for(let i  = 0;i<s.length;i++){
 
+        if(s[i] >= '0' && s[i] <= '9'){
+            if(s[i] > max){
+                 scMax = max;
+                 max = s[i];
+            }else{
+                if(s[i] > scMax && s[i] < max){
+                    scMax = s[i];
+                }
+            }
+        }
+    }
+    return scMax;
+};
 
-console.log('DAY_157 HASHING  ➡️ 15/02/2026');
+console.log(secondHighest("abc1111"));
