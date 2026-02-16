2a08b0416952ee9b6cf89ca4166eea3d5c1f7264
jk-nishad-72 <jkn18869@gmail.com>
2026-02-16 08:39:47 +0530

    done

--- Daywise_solutions/DSA_DAY158-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY158-ARRAY/app.js
@@ -1,4 +1,19 @@
+/*
+*485. Max Consecutive Ones
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+     let count = 0;
+     let max = 0;
+     for(let i = 0;i<nums.length;i++){
+               if(nums[i] === 1 ){
+                 count = count + 1;
+                max = Math.max(max  , count)
+               }else  if (nums[i] === 0 ) {
+                  count = 0;
+               }
+     }
 
-
-
-console.log('DAY_158 ARRAY➡️ 16/02/2026');
+     return max
+};
