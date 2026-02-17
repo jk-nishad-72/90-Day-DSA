a4f916fe815876bb983cd6fbcd07b27ba5b10734
jk-nishad-72 <jkn18869@gmail.com>
2026-02-17 22:30:23 +0530

    done

--- Daywise_solutions/DSA_DAY159-HASHING/app.js
+++ Daywise_solutions/DSA_DAY159-HASHING/app.js
@@ -1,3 +1,23 @@
+/*
+*167. Two Sum II - Input Array Is Sorted
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    let map = new Map();
+    let ans = [-1,-1]; 
 
+    for(let i = 0; i < numbers.length ;i++){
+            if(map.has(target - numbers[i])) {
+                ans[0] = (  map.get(target - numbers[i]) + 1) ;
+                ans[1] = i + 1;
+                return ans
+            }else{
+                map.set(numbers[i],i)
+            }
+    }
+};
 
-console.log('DAY_159 HASHING 17/02/2026');
+console.log(twoSum([2,7,11,15], 9));
