12c67555d29bda3788da7eb02693d092590645ea
jk-nishad-72 <jkn18869@gmail.com>
2026-02-18 23:25:04 +0530

    done

--- Daywise_solutions/DSA_DAY160-HASHING/app.js
+++ Daywise_solutions/DSA_DAY160-HASHING/app.js
@@ -1,3 +1,32 @@
+/**414. Third Maximum Number
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    
+    let max1 = -Infinity;
+    let max2 = -Infinity;
+    let max3 = -Infinity;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > max1){
+            max3 = max2;
+            max2 = max1;
+            max1 = nums[i];
+        } else if(nums[i] > max2 && nums[i] < max1){
+            max3 = max2;
+            max2 = nums[i];
+        } else if(nums[i] > max3 && nums[i] < max2){
+            max3 = nums[i];
+        }
+    }
+    if(max3 === -Infinity){
+        return max1;
+    } else {
+        return max3;
+    }
+    
+ 
+};
 
 
-console.log('DAY_160 HASHING ➡️ 1802202');
+console.log(thirdMax([1,2,-2147483648]));
