68371ca15aafdf3e9a83f57868d464faa71a9033
jk-nishad-72 <jkn18869@gmail.com>
2026-02-23 23:27:35 +0530

    brute force done

--- Daywise_solutions/DSA_DAY165-BINARY-SEARCH/app.js
+++ Daywise_solutions/DSA_DAY165-BINARY-SEARCH/app.js
@@ -1,3 +1,24 @@
+/**153. Find Minimum in Rotated Sorted Array
+ * @param {number[]} nums
+ * @return {number}
+ */
 
 
-console.log('DAY_165 BINARY SEARCH ➡️ 23022026');
+// Brute Force Solution
+var findMin = function(nums) {
+  
+    let min = nums[0];
+    for(let i = 1;i<nums.length;i++){
+         
+          if(nums[i] < min){
+             min = nums[i]
+          }
+    }
+    return min
+};
+
+
+
+
+
+console.log(findMin([4,5,6,7,0,1,2]));
