403970492423eab3d0ebed06472dfa1d178468be
jk-nishad-72 <jkn18869@gmail.com>
2026-02-25 21:20:28 +0530

    DONE

--- Daywise_solutions/DSA_DAY167-BINARY-SEARCH/app.js
+++ Daywise_solutions/DSA_DAY167-BINARY-SEARCH/app.js
@@ -1,2 +1,19 @@
 
-console.log('DAY_167 BINARY SEARCH ➡️ 25022026');
+/**162. Find Peak Element
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    
+    let f = 0;
+    let l = nums.length-1;
+    while(f < l) {
+         let m = Math.floor((f+l)/2);
+         if( nums[m] <= nums[m+1]) f = m+1;
+         else l = m;
+
+    }
+    return f
+};
+
+console.log(findPeakElement([1,2,1,3,5,6,4]));
