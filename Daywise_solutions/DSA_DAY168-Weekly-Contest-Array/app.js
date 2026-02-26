e76321590de107e3d57005283e010ca9d315c222
jk-nishad-72 <jkn18869@gmail.com>
2026-02-26 20:53:46 +0530

    done

--- Daywise_solutions/DSA_DAY168-Weekly-Contest-Array/app.js
+++ Daywise_solutions/DSA_DAY168-Weekly-Contest-Array/app.js
@@ -1,3 +1,17 @@
+/*
+ *2951. Find the Peaks
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function(mountain) {
+   let ans = [];
+   for(let i = 1 ;i< mountain.length-1;i++){
+     if(mountain[i]> mountain[i-1] && mountain[i] > mountain[i+1]){
+         
+          ans.push(i)
+     }
+   }
+   return ans;    
+};
 
-
-console.log('DAY_168 Weekly Contest Array ➡️ 26022026');
+console.log(findPeaks([1,4,3,8,5]));
