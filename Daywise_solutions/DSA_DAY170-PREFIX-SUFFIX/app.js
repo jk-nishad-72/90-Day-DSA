a9b4177283561bfe02b60aa3e0eb0b2577304709
jk-nishad-72 <jkn18869@gmail.com>
2026-02-28 21:22:27 +0530

    DONE

--- Daywise_solutions/DSA_DAY170-PREFIX-SUFFIX/app.js
+++ Daywise_solutions/DSA_DAY170-PREFIX-SUFFIX/app.js
@@ -1,3 +1,22 @@
+/**238. Product of Array Except Self
 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let n = nums.length;
+    let ans = new Array(n);
+    
+    ans[0] =  1;
+    for(let i = 1;i<n;i++){
+         ans[i] = ans[i-1] * nums[i-1];
+    }
 
-console.log('DAY_170 PREFIX - SUFFIX ➡️ 28/02/2026');
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        ans[i] = ans[i] * suffix;
+        suffix *= nums[i];
+    }
+return ans
+};
+console.log(productExceptSelf( [1,2,3,4]));
