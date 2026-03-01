94126c1f43c9b4c378e6cb253adcf919ff1301dd
jk-nishad-72 <jkn18869@gmail.com>
2026-03-01 23:20:30 +0530

    DONE

--- Daywise_solutions/DSA_DAY171-BINARY-SEARCH/app.js
+++ Daywise_solutions/DSA_DAY171-BINARY-SEARCH/app.js
@@ -1,3 +1,23 @@
 
+/**2089. Find Target Indices After Sorting Array
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 
-console.log('2089. Find Target Indices After Sorting Array');
+// use Binary Search to find the first and last index of the target in the sorted array
+var targetIndices = function(nums, target) {
+    
+    let sorted = nums.sort((a,b) => a - b);
+    let result = [];
+    for(let i = 0; i < sorted.length; i++) {
+        if(sorted[i] === target) {
+            result.push(i);
+        }
+    }
+    return result; 
+    
+};
+
+console.log(targetIndices([1,2,5,2,3], target = 2));
+
