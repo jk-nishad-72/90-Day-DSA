a6c08ff5bbe7ac1a50d07a3a113b545ba2a2f922
jk-nishad-72 <jkn18869@gmail.com>
2026-03-04 22:37:27 +0530

    done

--- Daywise_solutions/DSA_DAY174-LINKED-LIST/app.js
+++ Daywise_solutions/DSA_DAY174-LINKED-LIST/app.js
@@ -1,2 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**61. Rotate List
 
-console.log('DAY_174 LINKED LIST  ➡️ 04032026');
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    
+    if (!head) return null;
+
+    let length = 1;
+    let tail = head;
+    while (tail.next) {
+        tail = tail.next;
+        length++;
+    }
+    k = k % length;
+    if (k === 0) return head;
+    let newTail = head;
+    for (let i = 0; i < length - k - 1; i++) {
+        newTail = newTail.next;
+    }
+    let newHead = newTail.next;
+    newTail.next = null;
+    tail.next = head;
+    return newHead;
+};
+
+
