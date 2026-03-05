3e4d1e882a868991d6352cf5c6fbcb3ad45afa24
jk-nishad-72 <jkn18869@gmail.com>
2026-03-05 23:31:03 +0530

    done

--- Daywise_solutions/DSA_DAY175-HASH-TABLE/app.js
+++ Daywise_solutions/DSA_DAY175-HASH-TABLE/app.js
@@ -1,3 +1,28 @@
 
+/**290. Word Pattern
 
-console.log('DAY_175 HASH TABLE ➡️ 05032026'); 
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    
+    let words = s.split(" ");
+    if(pattern.length !== words.length) return false;
+
+    let map = new Map();
+
+    for(let i = 0; i < pattern.length; i++){
+        let char = pattern[i];
+        let word = words[i];
+        if(map.has(char)){
+            if(map.get(char) !== word) return false;
+        } else {
+            if([...map.values()].includes(word)) return false;
+            map.set(char, word);
+        }   
+    }
+    return true;
+};
+
+console.log(wordPattern( pattern = "abba", s = "dog cat cat dog"));
