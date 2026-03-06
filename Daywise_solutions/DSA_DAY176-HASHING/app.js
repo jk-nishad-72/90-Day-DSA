b13f9d702bf295f171f96bf5be37c76a5cd57ac3
jk-nishad-72 <jkn18869@gmail.com>
2026-03-06 23:46:09 +0530

    done

--- Daywise_solutions/DSA_DAY176-HASHING/app.js
+++ Daywise_solutions/DSA_DAY176-HASHING/app.js
@@ -1,4 +1,39 @@
+/**890. Find and Replace Pattern
+ * @param {string[]} words
+ * @param {string} pattern
+ * @return {string[]}
+ */
+var findAndReplacePattern = function(words, pattern) {
+    
+    let result = [];
+    let patternMap = new Map();
+    let patternSet = new Set();
+    for(let i = 0; i < pattern.length; i++){
+        if(!patternMap.has(pattern[i])){
+            patternMap.set(pattern[i], i);
+            patternSet.add(pattern[i]);
+        }
+    }
+    
+    for(let word of words){
+        let wordMap = new Map();
+        let wordSet = new Set();    
+        let isMatch = true; 
+        for(let i = 0; i < word.length; i++){
+            if(!wordMap.has(word[i])){  
+                wordMap.set(word[i], i);
+                wordSet.add(word[i]);
+            }   
+            if(patternMap.get(pattern[i]) !== wordMap.get(word[i])){
+                isMatch = false;
+                break;
+            }   
+        }        if(isMatch && patternSet.size === wordSet.size){
+            result.push(word);
+        }   
+    }
+    return result;
+    
+};
 
-
-
-console.log('DAY_176 HASHING ➡️ 06032026');
+console.log(findAndReplacePattern( words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"));
