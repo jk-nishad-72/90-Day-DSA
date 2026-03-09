fce6eb7861b459139f05d9e8c5d361932f1b1449
jk-nishad-72 <jkn18869@gmail.com>
2026-03-09 19:30:05 +0530

    done

--- Daywise_solutions/DSA_DAY179-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY179-ARRAY/app.js
@@ -1,2 +1,37 @@
 
-console.log('DAY_179 ARRAY➡️ 09032026');
+
+/*
+ *697. Degree of an Array
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+    let map = new Map();
+    let degree = 0;
+    let minLength = 0;
+    for(let i = 0; i < nums.length; i++){   
+        if(map.has(nums[i])){
+            let obj = map.get(nums[i]);
+            obj.count++;
+            obj.end = i;
+            map.set(nums[i], obj);
+        }   
+        else{
+            map.set(nums[i], {count: 1, start: i, end: i});
+        }
+    }
+    for(let [key, value] of map){
+        if(value.count > degree){
+            degree = value.count;
+            minLength = value.end - value.start + 1;
+        }
+        else if(value.count === degree){
+            minLength = Math.min(minLength, value.end - value.start + 1);
+        }
+    }
+    return minLength;
+};
+
+console.log(findShortestSubArray([1,2,2,3,1,4,2]));
+
+
