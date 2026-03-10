b07d3386d57a663424635cac0b3e56e9b95ffa67
jk-nishad-72 <jkn18869@gmail.com>
2026-03-10 20:02:11 +0530

    done

--- Daywise_solutions/DSA_DAY180-MATHS/app.js
+++ Daywise_solutions/DSA_DAY180-MATHS/app.js
@@ -1,4 +1,24 @@
+/**204. Count Primes
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    
+    if (n < 2) return 0;
+    
+    let count = 0;
+    const isPrime = new Array(n).fill(true);
+    isPrime[0] = isPrime[1] = false;
+    
+    for (let i = 2; i < n; i++) {
+        if (isPrime[i]) {
+            count++;
+            for (let j = i * 2; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return count;
+};
 
-
-
-console.log('DAY_180 MATHS 10032026');
+console.log(countPrimes(n = 10));
