8435714d1546f0061ab6e25c8d5a063f31ed715e
jk-nishad-72 <jkn18869@gmail.com>
2026-03-11 21:15:45 +0530

    DONE

--- Daywise_solutions/DSA-DAY181-MATHS/app.js
+++ Daywise_solutions/DSA-DAY181-MATHS/app.js
@@ -1,4 +1,24 @@
+/*
+ *263. Ugly Number
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    
+    if (n <= 0) return false;
+    
+    const primeFactors = [2, 3, 5];
+    for (let factor of primeFactors) {
+        while (n % factor === 0) {
+            n /= factor;
+        }                       
+    }
+    
+    return n === 1;
 
+};
 
-console.log('DSA DAY 181 MATHS ');
-
+// Test cases
+console.log(isUgly(6)); // true
+console.log(isUgly(1)); // true
+console.log(isUgly(14)); // false
