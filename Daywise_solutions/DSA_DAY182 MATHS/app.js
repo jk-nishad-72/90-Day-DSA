504e60b619728f8ecd0483bf7df4608b91b40eba
jk-nishad-72 <jkn18869@gmail.com>
2026-03-12 22:23:23 +0530

    done

--- Daywise_solutions/DSA_DAY182 MATHS/app.js
+++ Daywise_solutions/DSA_DAY182 MATHS/app.js
@@ -1,3 +1,29 @@
+/**264. Ugly Number II
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+    
+    if (n <= 0) return 0;
+
+    const uglyNumbers = [1];    
+    let i2 = 0, i3 = 0, i5 = 0;
+
+    for (let i = 1; i < n; i++) {
+        const next2 = uglyNumbers[i2] * 2;
+        const next3 = uglyNumbers[i3] * 3;
+        const next5 = uglyNumbers[i5] * 5;
+        const nextUgly = Math.min(next2, next3, next5);
+
+        uglyNumbers.push(nextUgly);
+        if (nextUgly === next2) i2++;
+        if (nextUgly === next3) i3++;
+        if (nextUgly === next5) i5++;
+    }
+
+    return uglyNumbers[n - 1];
 
 
-console.log('DAY_182 MATHS');
+};
+
+console.log(nthUglyNumber(1)); // Output: 12
