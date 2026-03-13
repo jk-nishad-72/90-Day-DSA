322b6b9470fb3ccafa926a73d2b1cb390fc49ff0
jk-nishad-72 <jkn18869@gmail.com>
2026-03-13 22:17:34 +0530

    TOPIC ADDED

--- Daywise_solutions/DSA_DAY183-MATHS/app.js
+++ Daywise_solutions/DSA_DAY183-MATHS/app.js
@@ -1,3 +1,18 @@
+/**326. Power of Three
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    
+    if (n < 1) return false;
+    
+    while (n % 3 === 0) {
+        n /= 3;
+    }
 
-
-console.log('DAY_183 MATHS');
+    return n === 1;
+};
+console.log(isPowerOfThree(27)); // true
+console.log(isPowerOfThree(0)); // false
+console.log(isPowerOfThree(9)); // true
+console.log(isPowerOfThree(45)); // false
