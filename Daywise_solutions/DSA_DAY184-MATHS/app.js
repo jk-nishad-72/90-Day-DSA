a28f2138824c61fd4ee3d08d647cb947189be027
jk-nishad-72 <jkn18869@gmail.com>
2026-03-14 23:19:22 +0530

    DONE

--- Daywise_solutions/DSA_DAY184-MATHS/app.js
+++ Daywise_solutions/DSA_DAY184-MATHS/app.js
@@ -1,3 +1,29 @@
+/**728. Self Dividing Numbers
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    
+    let result = [];
 
+    for (let i = left; i <= right; i++) {
+        let num = i;
+        let isSelfDividing = true;
+        while (num > 0) {
+            let digit = num % 10;
+            if (digit === 0 || i % digit !== 0) {
+                isSelfDividing = false;
+                break;
+            }
+            num = Math.floor(num / 10);
+        }
+        if (isSelfDividing) {
+            result.push(i);
+        }
+    }
 
-console.log('DAY_184 MATHS ➡️ 14032026 ');
+    return result;
+};
+
+console.log(selfDividingNumbers(left = 1, right = 22));
