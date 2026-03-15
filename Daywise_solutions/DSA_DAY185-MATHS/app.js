a5e923066403ff27d2eab75c8ad09052d3589eab
jk-nishad-72 <jkn18869@gmail.com>
2026-03-15 23:28:58 +0530

    done

--- Daywise_solutions/DSA_DAY185-MATHS/app.js
+++ Daywise_solutions/DSA_DAY185-MATHS/app.js
@@ -1,4 +1,22 @@
+/**367. Valid Perfect Square
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    
+    if(num < 0) return false;
+    if(num === 0 || num === 1) return true;
+    let left = 1;
+    let right = num;
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(mid * mid === num) return true;
+        else if(mid * mid < num) left = mid + 1;
+        else right = mid - 1;   
+    }
+    return false;
+    
 
+};
 
-
-console.log('DAY_185 MATHS ➡️ 15032026');
+console.log(isPerfectSquare( num = 16));
