ac9758c4ef797a1818c07e4b2a8d267fdd4d6d0e
jk-nishad-72 <jkn18869@gmail.com>
2026-03-18 23:43:14 +0530

    done

--- Daywise_solutions/DSA_DAY188-HASHING/app.js
+++ Daywise_solutions/DSA_DAY188-HASHING/app.js
@@ -1,4 +1,30 @@
+/**409. Longest Palindrome
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    
+    let charCount = {};
+    let longest = 0;
+    let hasOddCount = false;
+    for (let char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
 
+    for (let char in charCount) {
+        if (charCount[char] % 2 === 0) {
+            longest += charCount[char];
+        } else {
+            longest += charCount[char] - 1;
+            hasOddCount = true;
+        }
+    }
+    if (hasOddCount) {
+        longest += 1;
+    }   
+    return longest;
+    
 
+};
 
-console.log('DAY_188 HASHING ➡️ 18032026');
+console.log(longestPalindrome(s = "abccccdd"));
