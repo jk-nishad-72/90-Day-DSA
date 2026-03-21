26c9d0876e4c6d783d913f0ad782eb7a633d1ef0
jk-nishad-72 <jkn18869@gmail.com>
2026-03-21 15:47:58 +0530

    done

--- Daywise_solutions/DSA_DAY191-LINKED-LIST/app.js
+++ Daywise_solutions/DSA_DAY191-LINKED-LIST/app.js
@@ -1,3 +1,41 @@
+//705. Design HashSet
+var MyHashSet = function() {
+    this.arr = [];
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    this.arr.push(key);
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    this.arr = this.arr.filter(el => el !== key);
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return this.arr.includes(key);
+};
 
 
-console.log('DAY_191 LINKED LIST ➡️ 21032026');
+//   Your MyHashSet object will be instantiated and called as such:
+  var obj = new MyHashSet()
+  obj.add(2)
+  obj.add(3)
+  obj.add(4)
+  obj.remove(2)
+ var param_3 = obj.contains(2)
+
+ console.log(obj ,   param_3);
+ 
+ 
