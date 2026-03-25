bca00a99ffb768528fc5f6e2c257756cfb604119
jk-nishad-72 <jkn18869@gmail.com>
2026-03-25 08:09:38 +0530

    done

--- Daywise_solutions/DSA_DAY195-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY195-TWO-POINTER/app.js
@@ -1,3 +1,20 @@
+/** 905. Sort Array By Parity
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 
-console.log('## DAY_195 TWO POINTER➡️ 25032026');
+ var swap = function(arr , i , j){
+    let temp = arr[i];
+    arr[i]  = arr[j];
+    arr[j] = temp;
+ }
+var sortArrayByParity = function(nums) {
+    
+    let i = 0 , j = 0;
 
+    while(i < nums.length){
+         if(nums[i] % 2 === 0)swap(nums , i++, j++)
+         else i++
+    }
+    return nums
+};
