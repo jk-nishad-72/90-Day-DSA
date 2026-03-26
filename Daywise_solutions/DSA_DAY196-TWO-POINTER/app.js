d7764263a264d3fbefab09f9d22eb635e70ba852
jk-nishad-72 <jkn18869@gmail.com>
2026-03-26 21:58:25 +0530

    done

--- Daywise_solutions/DSA_DAY196-TWO-POINTER/app.js
+++ Daywise_solutions/DSA_DAY196-TWO-POINTER/app.js
@@ -1,5 +1,25 @@
 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    let i = 0 , j = 1 ;
+    while(i < nums.length && j < nums.length){
+        if(nums[i] % 2 === 0){
+            i += 2 ;
+        }
+        else if(nums[j] % 2 === 1){
+            j += 2 ;
+        }
+        else{
+            let temp = nums[i] ;
+            nums[i] = nums[j] ;
+            nums[j] = temp ;
+            i += 2 ;
+            j += 2 ;
+        }
+    }
+    return nums ;
+};
 
-
-
-console.log('DAY_196 TWO POINTER➡️ 26032026');
