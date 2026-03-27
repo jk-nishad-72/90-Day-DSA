ecc3f00b79ba077d932fd3a70e52639f34b82aa9
jk-nishad-72 <jkn18869@gmail.com>
2026-03-27 23:15:42 +0530

    solve

--- Daywise_solutions/DSA-DAY197-ARRAY/app.js
+++ Daywise_solutions/DSA-DAY197-ARRAY/app.js
@@ -1,3 +1,29 @@
+/**2164. Sort Even and Odd Indices Independently
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortEvenOdd = function(nums) {
+    let even = [];
+    let odd = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            even.push(nums[i]);
+        } else {
+            odd.push(nums[i]);
+        }
+    }
+    even.sort((a, b) => a - b);
+    odd.sort((a, b) => b - a);
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            result.push(even.shift());
+        } else {
+            result.push(odd.shift());
+        }
+    }
+    return result;
 
+};
 
-console.log('DAY_197 ARRAY➡️ 27032026');
+console.log(sortEvenOdd(nums = [4,1,2,3]));
