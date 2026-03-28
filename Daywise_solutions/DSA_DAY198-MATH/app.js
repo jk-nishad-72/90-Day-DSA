476ff659f4f6e270b3ae78d7d26d48afea765aec
jk-nishad-72 <jkn18869@gmail.com>
2026-03-28 22:17:01 +0530

    solve

--- Daywise_solutions/DSA_DAY198-MATH/app.js
+++ Daywise_solutions/DSA_DAY198-MATH/app.js
@@ -1,4 +1,33 @@
+/**628. Maximum Product of Three Numbers
+
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    
+    let product1 = nums[0] * nums[1] * nums[2];
+
+    for(let i = 0; i < nums.length - 2; i++) {
+        for(let j = i + 1; j < nums.length - 1; j++) {
+            for(let k = j + 1; k < nums.length; k++) {
+                product1 = Math.max(product1, nums[i] * nums[j] * nums[k]);
+            }
+        }    }
+
+    return product1;
 
 
+   
+};
 
-console.log('DAY_198 MATH➡️ 28032026 ');
+/*optimal solution
+
+ * 
+ */
+var maximumProduct = function(nums) {
+    nums.sort( (a,b)=> a-b)
+    let n = nums.length;
+    return Math.max(nums[0]*nums[1]*nums[n-1] , nums[n-1] * nums[n-2] * nums[n-3])
+};
+
+console.log(maximumProduct([-1 , -2 , -3]));
