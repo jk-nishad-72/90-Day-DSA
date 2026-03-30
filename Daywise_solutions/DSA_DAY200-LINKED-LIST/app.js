d81a7be481f16b3bcfaf004db5003d92380d2753
jk-nishad-72 <jkn18869@gmail.com>
2026-03-30 23:26:28 +0530

    optimize solve

--- Daywise_solutions/DSA_DAY200-LINKED-LIST/app.js
+++ Daywise_solutions/DSA_DAY200-LINKED-LIST/app.js
@@ -18,19 +18,36 @@
  }
 var modifiedList = function(nums, head) {
     
+
+    //this is time limit exceeded because we are using includes method which is O(n) and we are doing it for each node in the linked list which is also O(n) so overall time complexity is O(n^2)
+    // let dummy = new ListNode(0);
+    // dummy.next = head;
+    // let current = dummy;
+    // while (current.next !== null) {
+    //     if (nums.includes(current.next.val)) {
+    //         current.next = current.next.next; // Skip the node to delete
+    //     } else {
+    //         current = current.next; // Move to the next node
+    //     }   
+    // }
+    // return dummy.next; // Return the modified list starting from the original head 
+
+
+    //To optimize the time complexity we can use a Set to store the values in nums for O(1) average time complexity lookups.
+    //  This way, we can achieve an overall time complexity of O(n) where n is the number of nodes in the linked list.
+    let numSet = new Set(nums);
     let dummy = new ListNode(0);
     dummy.next = head;
     let current = dummy;
-    
     while (current.next !== null) {
-        if (nums.includes(current.next.val)) {
-            current.next = current.next.next;
+        if (numSet.has(current.next.val)) {
+            current.next = current.next.next; // Skip the node to delete
         } else {
-            current = current.next;
+            current = current.next; // Move to the next node
         }
     }
-    
-    return dummy.next;
+    return dummy.next; // Return the modified list starting from the original head
+
 
 };
 
