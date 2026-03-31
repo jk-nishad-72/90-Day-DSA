3204fb77e7b264a781a18e8ec0d9f27b34748c92
jk-nishad-72 <jkn18869@gmail.com>
2026-03-31 16:44:12 +0530

    solved

--- Daywise_solutions/DSA_DAY201-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY201-ARRAY/app.js
@@ -1,2 +1,23 @@
+/*
+*3190. Find Minimum Operations to Make All Elements Divisible by Three
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let i = 0;
+    let count = 0;
+    while(i< nums.length){
+         if(nums[i] % 3 === 0){ i++}
+         else if(nums[i] % 3 === 1){
+             nums[i] -= 1;
+             count++
+         }else{
+            nums[i] += 1;
+            count++
+         }
+    }
+    return count
+};
 
-console.log('DAY_201 ARRAY ➡️ 31032026');
+
+console.log(minimumOperations([1 , 2, 3 ,4]));
