9fea961c202960586797495faaf07a83ca728b41
jk-nishad-72 <jkn18869@gmail.com>
2026-04-01 16:37:40 +0530

    solved

--- Daywise_solutions/DSA_DAY202-MATHS/app.js
+++ Daywise_solutions/DSA_DAY202-MATHS/app.js
@@ -1,2 +1,19 @@
+/**453. Minimum Moves to Equal Array Elements
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function(nums) {
+    let sum = 0;
+    let min = nums[0];
+    
+    for(let i = 0;i<nums.length;i++){
+         sum += nums[i];
+         min = Math.min(min , nums[i])
+    }
 
-console.log('DAY_202 MATHS ➡️ 01042026');
+   return (sum - (min * nums.length))
+};
+
+console.log(minMoves([1,2,3]));
+console.log(minMoves([1,1,1]));
+
