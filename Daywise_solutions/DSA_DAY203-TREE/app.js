28f11cbc32c0e688ae6696d28aa59525f4088a39
jk-nishad-72 <jkn18869@gmail.com>
2026-04-02 22:35:10 +0530

    solve

--- Daywise_solutions/DSA_DAY203-TREE/app.js
+++ Daywise_solutions/DSA_DAY203-TREE/app.js
@@ -1,4 +1,31 @@
 
+/**617. Merge Two Binary Trees
+ 
+ */
+/**
+ * 
+* Definition for a binary tree node.
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
 
 
-console.log('DAY_203 TREE ➡️ 02042026');
+ function TreeNode(val, left, right) {
+     this.val = (val===undefined ? 0 : val)
+     this.left = (left===undefined ? null : left)
+     this.right = (right===undefined ? null : right)
+ }
+var mergeTrees = function(root1, root2) {
+    if(root1 === null ) return root2;
+    if(root2 === null ) return root1;
+
+    let newNode = new TreeNode(root1.val + root2.val)
+
+    newNode.left = mergeTrees(root1.left , root2.left)
+    newNode.right = mergeTrees(root1.right , root2.right)
+
+    return newNode;
+};
+
+//  
