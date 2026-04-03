c53efe56d68def2c5a6fc06ced7df3d1acc1a824
jk-nishad-72 <jkn18869@gmail.com>
2026-04-03 23:33:41 +0530

    solved

--- Daywise_solutions/DSA_DAY204-TREE/app.js
+++ Daywise_solutions/DSA_DAY204-TREE/app.js
@@ -1,3 +1,27 @@
+/**2331. Evaluate Boolean Binary Tree
+ * Definition for a binary tree node.
+ *
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+ function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+var evaluateTree = function(root) {
+    
+    if(root.left === null && root.right === null) return root.val == 1;
+
+    const left = evaluateTree(root.left )
+    const right = evaluateTree(root.right )
+
+    return (root.val === 2 ? left || right : left && right ) 
+
+}
 
 
-console.log('DAY_204 TREE ➡️ 03042026');
