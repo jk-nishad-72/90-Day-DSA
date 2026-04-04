9490bad8d074da7de14fe34782ccda1315f084a1
jk-nishad-72 <jkn18869@gmail.com>
2026-04-04 23:56:12 +0530

    solved

--- Daywise_solutions/DSA_DAY205-BFS/app.js
+++ Daywise_solutions/DSA_DAY205-BFS/app.js
@@ -1,3 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**103. Binary Tree Zigzag Level Order Traversal
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    
+     let ans = [];
+     let leftToRight = true;
+    if(root == null ) return ans
+    let q = [];
+    q.push(root)
+    while(q.length != 0 ){
+         let size = q.length;
+         let cur = [];
+         for(let i = 0;i<size;i++){
+             let node = q.shift();             
+             if(leftToRight){
+                 cur.push(node.val)
+             }else{
+                 cur.unshift(node.val)
+             }
+             if(node.left != null) q.push(node.left)
+             if(node.right != null) q.push(node.right)
+         }
+        ans.push(cur) 
+        leftToRight = !leftToRight;
 
-
-console.log('DAY_205 BFS ➡️ 04042026');
+    }
+   return ans
+};
