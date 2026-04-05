3463936ad840aee8a5a2d7efef6d9376126630f0
jk-nishad-72 <jkn18869@gmail.com>
2026-04-05 23:54:54 +0530

    solve

--- Daywise_solutions/DSA_DAY206-MATRIX/app.js
+++ Daywise_solutions/DSA_DAY206-MATRIX/app.js
@@ -1,3 +1,37 @@
+/**3417. Zigzag Grid Traversal With Skip
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+var zigzagTraversal = function(grid) {
+    
+    const m = grid.length;
+    const n = grid[0].length;
+    const result = [];
+    let leftToRight = true;
+    let take = true;
 
+    for (let i = 0; i < m; i++) {
+        if (leftToRight) {
+            for (let j = 0; j < n; j++) {
+                if (take) {
+                    result.push(grid[i][j]);
+                }
+                take = !take;
 
-console.log('DAY_206 MATRIX ➡️ 05042026');
+            }
+        } else {
+            for (let j = n - 1; j >= 0; j--) {
+                if (take) { 
+
+                result.push(grid[i][j]);
+                }               
+                 take = !take;
+            }
+        }
+        leftToRight = !leftToRight;
+    }
+    return result;
+
+};
+
+console.log(zigzagTraversal(  [[1,2],[3,4]] )); // Output: [1,3,5,7,9]
