6d84acf6eab64a72839ed4340b90ca89e6f0e404
jk-nishad-72 <jkn18869@gmail.com>
2026-04-08 23:54:03 +0530

    solve

--- Daywise_solutions/DSA_DAY209-BTREE/app.js
+++ Daywise_solutions/DSA_DAY209-BTREE/app.js
@@ -1,3 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ *
+ */
+/*
+ *671. Second Minimum Node In a Binary Tree
+ * @param {TreeNode} root
+ * @return {number}
+ */
 
+  function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right) }
 
-console.log('DAY_209 TREE ➡️ 08042026');
+var findSecondMinimumValue = function(root) {
+    
+    if(!root) return -1;
+    
+    let min = root.val;
+    let secondMin = -1;
+    const dfs = (node) => {
+        if(!node) return;
+        if(node.val > min) {
+            if(secondMin === -1 || node.val < secondMin) {
+                secondMin = node.val;
+            }
+        }
+        dfs(node.left);
+        dfs(node.right);
+    };
+    dfs(root);
+    return secondMin;
+
+};
