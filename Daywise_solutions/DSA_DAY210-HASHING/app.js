abc07b33baf4b7b4547f78eeaccbb29d9fd17e73
jk-nishad-72 <jkn18869@gmail.com>
2026-04-09 23:11:09 +0530

    solve

--- Daywise_solutions/DSA_DAY210-HASHING/app.js
+++ Daywise_solutions/DSA_DAY210-HASHING/app.js
@@ -1,3 +1,21 @@
+/**DAY_210 HASHING ➡️ 09042026
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let map = new Map();
+    let set = new Set();
 
+      for(let num of arr){
+         map.set(num , map.get(num) + 1 || 1)
+      }
 
-console.log('DAY_210 HASHING ➡️ 09042026');
+      for(let [key ,value] of map ){
+
+         if(set.has(value)) return false
+         else set.add(value)
+      }
+      return true
+};
+
+console.log(uniqueOccurrences([1,2 ,2 ,1, 1, 3]));
