1371d83aa64de9e4f2f086f4b881621bbc03597a
jk-nishad-72 <jkn18869@gmail.com>
2026-04-10 23:23:18 +0530

    solved

--- Daywise_solutions/DSA_DAY211-HASHING/app.js
+++ Daywise_solutions/DSA_DAY211-HASHING/app.js
@@ -1,2 +1,23 @@
 
-console.log("DAY_211 HASHING ➡️ 10042026");
+/*
+ *1331. Rank Transform of an Array
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    
+    let sorted = [...new Set(arr)].sort((a, b) => a - b);
+    let rankMap = new Map();
+    sorted.forEach((num, index) => {
+        rankMap.set(num, index + 1);
+    });
+
+    return arr.map(num => rankMap.get(num));
+
+};
+
+console.log(arrayRankTransform([40, 10, 20, 30]));
+console.log(arrayRankTransform([100, 100, 100]));
+
+console.log(arrayRankTransform([37, 12, 28, 9, 100, 56, 80, 5, 12]));
+
