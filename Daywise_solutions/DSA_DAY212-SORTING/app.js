87d56cf304df6ed16281356e450ac6122673d719
jk-nishad-72 <jkn18869@gmail.com>
2026-04-11 23:47:49 +0530

    solve

--- Daywise_solutions/DSA_DAY212-SORTING/app.js
+++ Daywise_solutions/DSA_DAY212-SORTING/app.js
@@ -1,3 +1,31 @@
+/**414. Third Maximum Number
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    
+    let max = -Infinity;
+    let secondMax = -Infinity;
+    let thirdMax = -Infinity;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > max){
+            thirdMax = secondMax;
+            secondMax = max;
+            max = nums[i];
+        } else if(nums[i] > secondMax && nums[i] < max){
+            thirdMax = secondMax;
+            secondMax = nums[i];
+        } else if(nums[i] > thirdMax && nums[i] < secondMax){
+            thirdMax = nums[i];
+        }
+    }
+    if(thirdMax === -Infinity){
+        return max;
+    } else {
+        return thirdMax;
+    }
 
+    
+};
 
-console.log('DAY_212 SORTING ➡️ 11042026');
+console.log(thirdMax( [3,2,1]));
