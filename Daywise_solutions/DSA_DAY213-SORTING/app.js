e1d73f1a874390018bf82680088440862d784bbc
jk-nishad-72 <jkn18869@gmail.com>
2026-04-12 16:14:58 +0530

    solve

--- Daywise_solutions/DSA_DAY213-SORTING/app.js
+++ Daywise_solutions/DSA_DAY213-SORTING/app.js
@@ -1,2 +1,23 @@
+/**2733. Neither Minimum nor Maximum
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function(nums) {
+    if(nums.length <= 2 ) return -1;
 
-console.log('DAY_213 SORTING ➡️ 12042026');
+    let max = -Infinity;
+    let min = nums[0];
+
+     for(let i = 0;i<nums.length ;i++){
+
+          if(nums[i] > max ) max = nums[i]
+          if(nums[i] < min ) min = nums[i]
+     }
+
+     for(let i = 0;i<nums.length;i++){
+
+         if(nums[i] != max && nums[i] != min ) return nums[i]
+     }
+};
+
+console.log(findNonMinOrMax(nums = [3,2,1,4])); 
