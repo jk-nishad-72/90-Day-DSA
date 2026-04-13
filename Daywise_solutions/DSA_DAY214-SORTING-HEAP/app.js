e5a614e5de73e13f9c0ae019268d8e1d03626651
jk-nishad-72 <jkn18869@gmail.com>
2026-04-13 15:25:07 +0530

    brute force solve

--- Daywise_solutions/DSA_DAY214-SORTING-HEAP/app.js
+++ Daywise_solutions/DSA_DAY214-SORTING-HEAP/app.js
@@ -1,3 +1,17 @@
+/**215. Kth Largest Element in an Array
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+// Approach 1: Sorting - Time Complexity: O(n log n) due to sorting the array brute force approach
+var findKthLargest = function(nums, k) {
+         nums.sort((a,b)=> b - a  )
+         return nums[k-1]
+};
 
 
-console.log('DAY_214 SORTINGHEAP ➡️ 13042026 ');
+console.log(findKthLargest(nums = [3,2,1,5,6,4], k = 2)); // Output: 5
+console.log(findKthLargest(nums = [3,2,3,1,2,4,5,5,6], k = 4)); // Output: 4
+
+// 
