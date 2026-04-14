3ffe8123ec67c9ba267ffdde77c9ceb063238642
jk-nishad-72 <jkn18869@gmail.com>
2026-04-14 19:45:15 +0530

    solve

--- Daywise_solutions/DSA_DAY215-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY215-ARRAY/app.js
@@ -1,3 +1,39 @@
+/**59. Spiral Matrix II
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    let top = 0, bottom = n-1;
+    let left = 0, right = n-1;
+    let ans = new Array(n).fill(0).map(() => new Array(n)); 
+    let num = 1
+    while(top <= bottom && left <= right){
+         //* left to right 
+         for(let i = left; i<= right ;i++){
+            ans[top][i] = num++
+         }
+         top++
+         //* top to bottom  
+         for(let k = top; k<= bottom ;k++){
+            ans[k][right] = num++
+         }
+         right--
+//*  right  to left 
+         if(top <= bottom){
+            for(let j = right; j>= left ;j--){
+           ans[bottom][j] = num++
+         }
+         bottom--
+         }
+         //*  b  to top 
+         if(left <= right){
+            for(let l = bottom; l>= top ;l--){
+            ans[l][left] = num++
+         }
+         left++
+         }
+    }
+    return ans
+};
 
-
-console.log('DAY_215 ARRAY ➡️ 14042026 ');
+console.log(generateMatrix(3));
