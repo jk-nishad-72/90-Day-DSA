2482813f48db20d515656346c06ca10a7efecf7c
jk-nishad-72 <jkn18869@gmail.com>
2026-04-16 23:41:49 +0530

    solve

--- Daywise_solutions/DSA_DAY217-BIT-MANIPULATION/app.js
+++ Daywise_solutions/DSA_DAY217-BIT-MANIPULATION/app.js
@@ -1,3 +1,26 @@
+/**371. Sum of Two Integers
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+ 
+      let sum = 0;
+    let carry = 0;
 
+    while(b !== 0 ){
+        // calculate sum
+         sum = a ^ b ;
+         // calculate the carry 
+         carry = (a & b) << 1;
+        // update the a and b 
+         a = sum
+         b = carry
+    }
+    return a || sum
+    
+   
+    
+};
 
-console.log('## DAY_217 BIT-MANIPULATION ➡️ 16042026');
+console.log(getSum(1,2));
