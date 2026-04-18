e1288f4ee1e3b0bb44df407ce00ee240deaf0074
jk-nishad-72 <jkn18869@gmail.com>
2026-04-18 23:03:39 +0530

    solved

--- Daywise_solutions/DSA_DAY219-STACK/app.js
+++ Daywise_solutions/DSA_DAY219-STACK/app.js
@@ -1,2 +1,58 @@
+//155. Min Stack
 
-console.log('DAY_219 STACK ➡️ 18042026');
+var MinStack = function() {
+    
+    this.array = [];
+    this.min = Number.MAX_VALUE;
+   
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+
+    this.array.push(val);
+    if (val < this.min) {
+        this.min = val;
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    const popped = this.array.pop();
+    if (popped === this.min) {
+        this.min = Math.min(...this.array);
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.array[this.array.length - 1];
+};
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.min;
+};
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ *
+ */
+ var obj = new MinStack()
+  obj.push(-2)
+  obj.push(0)
+  obj.push(-3)  
+  obj.pop()
+  var param_3 = obj.top()
+  var param_4 = obj.getMin()
+
+  console.log(param_3, param_4);
+  
