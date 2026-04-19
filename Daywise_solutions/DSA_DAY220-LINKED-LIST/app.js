874175091aa3d4d08f5f868daa68d0ae6b399798
jk-nishad-72 <jkn18869@gmail.com>
2026-04-19 23:43:26 +0530

    solve

--- Daywise_solutions/DSA_DAY220-LINKED-LIST/app.js
+++ Daywise_solutions/DSA_DAY220-LINKED-LIST/app.js
@@ -1,3 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ *
+ */
+/**147. Insertion Sort List
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 
+ function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+ }
+var insertionSortList = function(head) {
+    
+    if(!head || !head.next) return head;
+    
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let tail = head.next;
+    let sortedTail = head;
 
-console.log('DAY_220 LINKED LIST ➡️ 19042026');
+    while(tail) {
+        let next = tail.next;
+        let prev = dummy;
+        while(prev.next !== tail && prev.next.val <= tail.val) {
+            prev = prev.next;
+        }
+        if(prev.next !== tail) {
+            sortedTail.next = tail.next;
+            tail.next = prev.next;
+            prev.next = tail;
+        } else {
+            sortedTail = tail;
+        }   
+        tail = next;
+    }
+    
+    return dummy.next;
+};
