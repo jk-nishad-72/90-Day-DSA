358f777c0666b79f219eef7646168f7769bb2322
jk-nishad-72 <jkn18869@gmail.com>
2026-04-20 23:59:10 +0530

    solved

--- Daywise_solutions/DSA_DAY221-STRING/app.js
+++ Daywise_solutions/DSA_DAY221-STRING/app.js
@@ -1,2 +1,23 @@
+/**709. To Lower Case
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    
+     
+    // return s.toLowerCase();
+    let result = '';
+    for(let i = 0; i < s.length; i++){
+        let charCode = s.charCodeAt(i);
+        if(charCode >= 65 && charCode <= 90){
+            result += String.fromCharCode(charCode + 32);
+        } else {
+            result += s[i];
+        }
+    }
+    return result;
 
-console.log('DAY_221 STRING ➡️ 20042026');
+};
+
+console.log(toLowerCase('Hello')); // Output: 'hello'
+console.log(toLowerCase('here'));
