6d5f9e245c7206e9fb208da2ef0af85956fd24d5
jk-nishad-72 <jkn18869@gmail.com>
2026-04-21 19:02:41 +0530

    solved

--- Daywise_solutions/DSA_DAY222-STRING/app.js
+++ Daywise_solutions/DSA_DAY222-STRING/app.js
@@ -1,3 +1,24 @@
+/**1684. Count the Number of Consistent Strings
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    
+    let count = 0;
+    for(let i = 0;i<words.length;i++){
+        for(let j = 0;j<words[i].length;j++){
+            // process.stdout.write(words[i][j] + "");
+          if(!allowed.includes(words[i][j])){
+              break;
+          }
+            if(j === words[i].length - 1){
+                // console.log("consistent string: " + words[i]);
+                count++;
+            }
+        }
+    }
+    return count;
+};
 
-
-console.log('DAY_222 STRING ➡️ 21042026');
+console.log(countConsistentStrings(allowed = "ab", words = ["ad","bd","aaab","baa","badab"])); //2
