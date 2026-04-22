5d36d445684b5a8aed8a76e849c258b6f778231e
jk-nishad-72 <jkn18869@gmail.com>
2026-04-22 23:28:13 +0530

    solved O(n^2) solution

--- Daywise_solutions/DSA_DAY223-STRING/app.js
+++ Daywise_solutions/DSA_DAY223-STRING/app.js
@@ -1,3 +1,31 @@
+/**1446. Consecutive Characters
+ * @param {string} s
+ * @return {number}
+ */
+
+// Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+// Return the power of the string. O(n^2) solution
+var maxPower = function(s) {
+    let power = 0;
+    let cur = 0;
+    if(s.length === 1) return 1;
+    for(let i = 0;i<s.length;i++){
+         for(let j = i;j<s.length;j++){
+             if(s[i] != s[j]){
+                 power = Math.max(power, cur)
+                 cur = 0;
+                 break;
+              } else cur++
+         }
+          power = Math.max(power, cur)
+          cur = 0;
+    }
+    return power 
+};
 
 
-console.log('DAY_223 STRING ➡️ 22042026');
+console.log(maxPower('cc'));
+console.log(maxPower('leeeetcode'));
+
+
