3ba61c7a4cb3858f0cb093f5522e1c0affab0a8a
jk-nishad-72 <jkn18869@gmail.com>
2026-04-23 22:42:27 +0530

    topic added

--- Daywise_solutions/DSA_DAY224-STRING/app.js
+++ Daywise_solutions/DSA_DAY224-STRING/app.js
@@ -1,3 +1,30 @@
+/**1844. Replace All Digits with Characters
+ * @param {string} s
+ * @return {string}
+ */
+ var shift = function(c , num ){
+     
+     let dig = c.charCodeAt(0) + Number(num);
+    // console.log(dig);
+    
+    // console.log(c + (String.fromCharCode(dig)));
+    
+     return c + (String.fromCharCode(dig))
+ }
+var replaceDigits = function(s) {
+    let result =  '';
 
+    for(let i = 1 ; i< s.length;i+=2){
+         result += shift(s[i-1] , s[i])
+    }
+     
+    if(s.length % 2 !== 0){
+        result += s[s.length - 1]
+    }
+    
+    return result
+};
 
-console.log('DAY_224 STRING ➡️ 23042026');
+console.log(replaceDigits('a1c1e1'));
+console.log(replaceDigits(s = "a1b2c3d4e"));
+
