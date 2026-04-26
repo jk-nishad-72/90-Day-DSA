5ca297d8b8c6bdfe99566d7a066db09c5ef04329
jk-nishad-72 <jkn18869@gmail.com>
2026-04-27 00:42:30 +0530

    d227

--- Daywise_solutions/DSA_DAY226-ARRAY/app.js
+++ Daywise_solutions/DSA_DAY226-ARRAY/app.js
@@ -23,10 +23,33 @@ class Solution {
 }
 
 // Do not edit the code below.
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let res = new Solution();
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let res = new Solution();
 
-console.log(res.sortHalf(arr));
+// console.log(res.sortHalf(arr));
+
+function capitalizeEnds(str) {
+    // Write your code here 
+    let words = str.split(' ')
+    // console.log(words)
+    let result = ''
+    for(let i = 0;i<words.length;i++){
+
+        if(words[i].length === 2) {
+            result += words[i].toLocaleUpperCase()+' '
+        }else {
+             result += words[i].slice(0,1).toLocaleUpperCase()
+                   + words[i].slice(1,words[i].length-1) 
+                   + words[i].slice(words[i].length-1 ,words[i].length).toLocaleUpperCase()
+                   +' '
+        }
+    }
+    return result
+}
+
+// console.log(capitalizeEnds('hello world'));
+
+
 
 
 
